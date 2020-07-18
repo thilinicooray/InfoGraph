ec88e630347292c050e064a32b2538b215de5805
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-19 03:13:10 +0800

    try vae

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -67,14 +67,14 @@ class GcnInfomax(nn.Module):
     node_kl_divergence_loss = torch.mean(
         - 0.5 * torch.sum(1 + node_logvar - node_mu.pow(2) - node_logvar.exp())
     )
-    node_kl_divergence_loss = 0.1 * node_kl_divergence_loss *num_graphs
+    node_kl_divergence_loss = node_kl_divergence_loss *num_graphs
     node_kl_divergence_loss.backward(retain_graph=True)
 
     # kl-divergence error for class latent space
     class_kl_divergence_loss = torch.mean(
         - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
     )
-    class_kl_divergence_loss *= num_graphs
+    class_kl_divergence_loss = 0.1 * class_kl_divergence_loss * num_graphs
     class_kl_divergence_loss.backward(retain_graph=True)
 
     # reconstruct samples
