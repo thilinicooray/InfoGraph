ee6b394e0c555703f5517cb97c2e47ffd6c46d0d
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-22 02:44:08 +0800

    set seed for pyt 11 c 10

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -132,7 +132,7 @@ class GcnInfomax(nn.Module):
 if __name__ == '__main__':
     
     args = arg_parse()
-    seed = 12
+    seed = 11
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
