580b3b0fc37368faa22a2326dda87ab71743de1a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-23 01:26:49 +0800

    seed 84 1 train 1 val

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
         print('Epoch {}, Recon Loss {} KL class Loss {} KL node Loss {}'.format(epoch, recon_loss_all / len(dataloader),
                                                                                            kl_class_loss_all / len(dataloader), kl_node_loss_all / len(dataloader)))
 
-        if epoch % log_interval == 0:
+        '''if epoch % log_interval == 0:
             model.eval()
             emb, y = model.get_embeddings(dataloader)
             res = evaluate_embedding(emb, y)
@@ -209,19 +209,19 @@ if __name__ == '__main__':
             accuracies['svc'].append(res[1])
             accuracies['linearsvc'].append(res[2])
             accuracies['randomforest'].append(res[3])
-            print(accuracies)
+            print(accuracies)'''
 
         #scheduler.step()
 
-    #model.eval()
-    '''
+    model.eval()
+
     emb, y = model.get_embeddings(dataloader)
     res = evaluate_embedding(emb, y)
     accuracies['logreg'].append(res[0])
     accuracies['svc'].append(res[1])
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])
-    print(accuracies)'''
+    print(accuracies)
 
     with open('unsupervised.log', 'a+') as f:
         s = json.dumps(accuracies)
