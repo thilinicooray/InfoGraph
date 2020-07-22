99c9a46273ecab5bdc20095bf000151edccc9a8d
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-23 00:36:21 +0800

    seed 84 1 train 1 val

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -182,13 +182,13 @@ if __name__ == '__main__':
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])
 
-    model.train()
+    #model.train()
     for epoch in range(1, epochs+1):
         recon_loss_all = 0
         kl_class_loss_all = 0
         kl_node_loss_all = 0
         mi_loss_all = 0
-
+        model.train()
         for data in dataloader:
             data = data.to(device)
             optimizer.zero_grad()
@@ -201,7 +201,7 @@ if __name__ == '__main__':
         print('Epoch {}, Recon Loss {} KL class Loss {} KL node Loss {}'.format(epoch, recon_loss_all / len(dataloader),
                                                                                            kl_class_loss_all / len(dataloader), kl_node_loss_all / len(dataloader)))
 
-        '''if epoch % log_interval == 0:
+        if epoch % log_interval == 0:
             model.eval()
             emb, y = model.get_embeddings(dataloader)
             res = evaluate_embedding(emb, y)
@@ -209,18 +209,18 @@ if __name__ == '__main__':
             accuracies['svc'].append(res[1])
             accuracies['linearsvc'].append(res[2])
             accuracies['randomforest'].append(res[3])
-            print(accuracies)'''
+            print(accuracies)
 
         #scheduler.step()
 
-    model.eval()
+    '''model.eval()
     emb, y = model.get_embeddings(dataloader)
     res = evaluate_embedding(emb, y)
     accuracies['logreg'].append(res[0])
     accuracies['svc'].append(res[1])
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])
-    print(accuracies)
+    print(accuracies)'''
 
     with open('unsupervised.log', 'a+') as f:
         s = json.dumps(accuracies)
