1d15efcfcc87be459993b05a485af4478b9fc786
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-23 15:17:10 +0800

    ptc setup2

--- new_unsupervised/evaluate_embedding.py
+++ new_unsupervised/evaluate_embedding.py
@@ -91,25 +91,28 @@ def logistic_classify(x, y):
     return np.mean(accs)
 
 def svc_classify(x, y, search):
-    kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=18)
-    accuracies = []
-    for train_index, test_index in kf.split(x, y):
 
-        x_train, x_test = x[train_index], x[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        # x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)
-        if search:
-            params = {'C':[0.001, 0.01,0.1,1,10,100,1000]}
-            classifier = GridSearchCV(SVC(), params, cv=5, scoring='accuracy', verbose=0)
-        else:
-            classifier = SVC(C=10)
-        classifier.fit(x_train, y_train)
-        accuracies.append(accuracy_score(y_test, classifier.predict(x_test)))
+    for i in range(100):
 
-    mean = np.mean(accuracies)
-    std = np.std(accuracies)
+        kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=i)
+        accuracies = []
+        for train_index, test_index in kf.split(x, y):
 
-    print('mean libsvm ', mean, 'std ', std)
+            x_train, x_test = x[train_index], x[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            # x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)
+            if search:
+                params = {'C':[0.001, 0.01,0.1,1,10,100,1000]}
+                classifier = GridSearchCV(SVC(), params, cv=5, scoring='accuracy', verbose=0)
+            else:
+                classifier = SVC(C=10)
+            classifier.fit(x_train, y_train)
+            accuracies.append(accuracy_score(y_test, classifier.predict(x_test)))
+
+        mean = np.mean(accuracies)
+        std = np.std(accuracies)
+
+        print('mean libsvm ', i, mean, 'std ', std)
 
     return mean
 
