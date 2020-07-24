cd263ded082e37563913b634d7ba26ef9a8dcbb3
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-24 20:07:12 +0800

    rdt b sig before bn in encoder

--- new_unsupervised/gin.py
+++ new_unsupervised/gin.py
@@ -66,11 +66,11 @@ class Encoder(torch.nn.Module):
             # if i == 2:
                 # feature_map = x2
         j = self.num_gc_layers
-        node_latent_space_mu = torch.sigmoid(self.bns[j](self.convs[j](x, edge_index)))
-        node_latent_space_logvar = torch.sigmoid(self.bns[j+1](self.convs[j+1](x, edge_index)))
+        node_latent_space_mu = self.bns[j](torch.sigmoid(self.convs[j](x, edge_index)))
+        node_latent_space_logvar = self.bns[j+1](torch.sigmoid(self.convs[j+1](x, edge_index)))
 
-        class_latent_space_mu = torch.sigmoid(self.bns[j+2](self.convs[j+2](x, edge_index)))
-        class_latent_space_logvar = torch.sigmoid(self.bns[j+3](self.convs[j+3](x, edge_index)))
+        class_latent_space_mu = self.bns[j+2](torch.sigmoid(self.convs[j+2](x, edge_index)))
+        class_latent_space_logvar = self.bns[j+3](torch.sigmoid(self.convs[j+3](x, edge_index)))
 
         '''node_latent_space_mu = F.relu(self.node_mu(x))
         node_latent_space_logvar = F.relu(self.node_logvar(x))
