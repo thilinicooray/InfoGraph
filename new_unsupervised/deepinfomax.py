f87c56db19aa7de70d9b920caf6ca7a0e80065fd
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-26 22:06:59 +0800

    debugging randomness

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -183,7 +183,7 @@ class GcnInfomax(nn.Module):
 
               class_emb = global_mean_pool(accumulated_class_latent_embeddings, batch)
 
-              print('clz emb ', class_emb[:5,:3])
+              #print('clz emb ', class_emb[:5,:3])
 
 
               ret.append(class_emb.cpu().numpy())
