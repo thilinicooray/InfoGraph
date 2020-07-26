bfe43f7caebf02c5c9204167fea289bdfd531b94
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-26 19:28:04 +0800

    debugging randomness

--- new_unsupervised/gin.py
+++ new_unsupervised/gin.py
@@ -57,6 +57,8 @@ class Encoder(torch.nn.Module):
         '''if x is None:
             x = torch.ones((batch.shape[0], 1)).to(device)'''
 
+        print('input x' , x[0,:5])
+
         xs = []
         for i in range(self.num_gc_layers):
 
