23e822e73ea49c5af27874b37d07566484948bc8
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-27 20:06:00 +0800

    using value clamp

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -306,13 +306,13 @@ if __name__ == '__main__':
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])'''
 
-    #model.train()
+    model.train()
     for epoch in range(1, epochs+1):
         recon_loss_all = 0
         kl_class_loss_all = 0
         kl_node_loss_all = 0
         mi_loss_all = 0
-        model.train()
+        #model.train()
         for data in dataloader:
             data = data.to(device)
 
@@ -339,7 +339,7 @@ if __name__ == '__main__':
 
 
 
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25)
             optimizer.step()
 
 
@@ -347,7 +347,7 @@ if __name__ == '__main__':
         print('Epoch {}, Recon Loss {} KL class Loss {} KL node Loss {}'.format(epoch, recon_loss_all / len(dataloader),
                                                                                            kl_class_loss_all / len(dataloader), kl_node_loss_all / len(dataloader)))
         #used during finetune phase
-        if epoch % log_interval == 0:
+        '''if epoch % log_interval == 0:
             model.eval()
             emb, y = model.get_embeddings(dataloader)
             res = evaluate_embedding(emb, y)
@@ -355,10 +355,10 @@ if __name__ == '__main__':
             accuracies['svc'].append(res[1])
             accuracies['linearsvc'].append(res[2])
             accuracies['randomforest'].append(res[3])
-            print(accuracies)
+            print(accuracies)'''
 
 
-    '''model.eval()
+    model.eval()
 
     for i in range(5):
     
@@ -368,7 +368,7 @@ if __name__ == '__main__':
         accuracies['svc'].append(res[1])
         accuracies['linearsvc'].append(res[2])
         accuracies['randomforest'].append(res[3])
-        print(accuracies)'''
+        print(accuracies)
 
     with open('unsupervised.log', 'a+') as f:
         s = json.dumps(accuracies)
