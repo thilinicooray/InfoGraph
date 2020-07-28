dc37e0da62476a3b545df34e5e1ffb459963ee7c
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-28 19:18:09 +0800

    setup 11 7

--- new_unsupervised/gin.py
+++ new_unsupervised/gin.py
@@ -91,19 +91,19 @@ class Decoder(torch.nn.Module):
     def __init__(self, node_dim, class_dim, feat_size):
         super(Decoder, self).__init__()
 
-        '''self.linear_model = torch.nn.Sequential(OrderedDict([
-            ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=node_dim, bias=True)),
-            ('relu_1', ReLU()),
-
-            ('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
-            ('relu_final', Tanh()),
-        ]))'''
-
         self.linear_model = torch.nn.Sequential(OrderedDict([
             ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=node_dim, bias=True)),
             ('relu_1', Tanh()),
 
+            ('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
+            ('relu_final', Tanh()),
         ]))
+
+        '''self.linear_model = torch.nn.Sequential(OrderedDict([
+            ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=node_dim, bias=True)),
+            ('relu_1', Tanh()),
+
+        ]))'''
 
     def forward(self, node_latent_space, class_latent_space, edge_index):
         x = torch.cat((node_latent_space, class_latent_space), dim=1)
