083a88d3b5c37895bffb3e555115a3fe078d6e3b
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-28 18:48:21 +0800

    setup 11 7

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -89,7 +89,7 @@ class GcnInfomax(nn.Module):
     #print('node kl unwei ', node_kl_divergence_loss, node_logvar, node_mu)
 
 
-    node_kl_divergence_loss = 0.000001*node_kl_divergence_loss *num_graphs
+    node_kl_divergence_loss = 0.00001*node_kl_divergence_loss *num_graphs
     #print('node kl wei ', node_kl_divergence_loss)
     node_kl_divergence_loss.backward(retain_graph=True)
 
@@ -98,7 +98,7 @@ class GcnInfomax(nn.Module):
         - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
     )
     #print('class kl unwei ', class_kl_divergence_loss)
-    class_kl_divergence_loss = 0.000001* class_kl_divergence_loss * num_graphs
+    class_kl_divergence_loss = 0.00001* class_kl_divergence_loss * num_graphs
     #print('class kl wei ', class_kl_divergence_loss)
     class_kl_divergence_loss.backward(retain_graph=True)
 
