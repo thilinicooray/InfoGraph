235c7d82b797fd34129105deabe5cbdd3b9d83ea
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-29 18:00:08 +0800

    setup 11 2

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -312,13 +312,13 @@ if __name__ == '__main__':
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])'''
 
-    model.train()
+    #model.train()
     for epoch in range(1, epochs+1):
         recon_loss_all = 0
         kl_class_loss_all = 0
         kl_node_loss_all = 0
         mi_loss_all = 0
-        #model.train()
+        model.train()
         for data in dataloader:
             data = data.to(device)
 
@@ -355,7 +355,7 @@ if __name__ == '__main__':
 
 
         #used during finetune phase
-        '''if epoch % log_interval == 0:
+        if epoch % log_interval == 0:
             model.eval()
             emb, y = model.get_embeddings(dataloader)
             res = evaluate_embedding(emb, y)
@@ -363,10 +363,10 @@ if __name__ == '__main__':
             accuracies['svc'].append(res[1])
             accuracies['linearsvc'].append(res[2])
             accuracies['randomforest'].append(res[3])
-            print(accuracies)'''
+            print(accuracies)
 
 
-    model.eval()
+    '''model.eval()
     
     #for i in range(5):
     emb, y = model.get_embeddings(dataloader)
@@ -375,7 +375,7 @@ if __name__ == '__main__':
     accuracies['svc'].append(res[1])
     accuracies['linearsvc'].append(res[2])
     accuracies['randomforest'].append(res[3])
-    print(accuracies)
+    print(accuracies)'''
 
     with open('unsupervised.log', 'a+') as f:
         s = json.dumps(accuracies)
