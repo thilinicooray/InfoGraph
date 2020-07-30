67bde33cbce9a0b728ccf2efa9bfbbe79a95e277
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-30 13:00:17 +0800

    s 11 2 final

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -89,7 +89,7 @@ class GcnInfomax(nn.Module):
     #print('node kl unwei ', node_kl_divergence_loss, node_logvar, node_mu)
 
 
-    node_kl_divergence_loss = 0.0000005*node_kl_divergence_loss *num_graphs
+    node_kl_divergence_loss = 0.0000001*node_kl_divergence_loss *num_graphs
     #print('node kl wei ', node_kl_divergence_loss)
 
 
@@ -98,7 +98,7 @@ class GcnInfomax(nn.Module):
         - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
     )
     #print('class kl unwei ', class_kl_divergence_loss)
-    class_kl_divergence_loss = 0.0000005* class_kl_divergence_loss * num_graphs
+    class_kl_divergence_loss = 0.0000001* class_kl_divergence_loss * num_graphs
     #print('class kl wei ', class_kl_divergence_loss)
 
 
@@ -116,7 +116,7 @@ class GcnInfomax(nn.Module):
     reconstructed_node = self.decoder(node_latent_embeddings, class_latent_embeddings, edge_index)
     
     #reconstruction_error =  mse_loss(reconstructed_node, x) * num_graphs
-    reconstruction_error = 0.05* self.recon_loss(reconstructed_node, edge_index, batch) * num_graphs
+    reconstruction_error = 0.01* self.recon_loss(reconstructed_node, edge_index, batch) * num_graphs
 
 
     #print(reconstruction_error.item(), class_kl_divergence_loss.item(), node_kl_divergence_loss.item())
