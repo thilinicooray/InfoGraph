863a2687b48ed126fb5f6a242f036b9e601d0989
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-31 21:37:39 +0800

    line graph

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -59,7 +59,7 @@ class GcnInfomax(nn.Module):
     # batch_size = data.num_graphs
 
     #node_mu, node_logvar, class_mu, class_logvar = self.encoder(x, edge_index, batch)
-    _, _, class_mu, class_logvar = self.encoder(x, edge_index, batch)
+    node_mu, node_logvar, class_mu, class_logvar = self.encoder(x, edge_index, batch)
 
 
     '''n_digits = 4
@@ -84,12 +84,13 @@ class GcnInfomax(nn.Module):
 
 
     # kl-divergence error for style latent space
-    '''node_kl_divergence_loss = torch.mean(
+    node_kl_divergence_loss = torch.mean(
         - 0.5 * torch.sum(1 + node_logvar - node_mu.pow(2) - node_logvar.exp())
     )
+    #print('node kl unwei ', node_kl_divergence_loss, node_logvar, node_mu)
 
 
-    node_kl_divergence_loss = 0.0000001*node_kl_divergence_loss *num_graphs'''
+    node_kl_divergence_loss = node_kl_divergence_loss *num_graphs
     #print('node kl wei ', node_kl_divergence_loss)
 
 
@@ -115,7 +116,7 @@ class GcnInfomax(nn.Module):
 
     #reconstructed_node = self.decoder(node_latent_embeddings, class_latent_embeddings, edge_index)
     
-    #reconstruction_error =  0.1*mse_loss(reconstructed_node, x) * num_graphs
+    #reconstruction_error =  mse_loss(reconstructed_node, x) * num_graphs
                             #+ self.recon_loss(class_latent_embeddings, edge_index, batch) * num_graphs
     reconstruction_error = self.recon_loss(class_latent_embeddings, edge_index, batch) * num_graphs
 
@@ -129,15 +130,14 @@ class GcnInfomax(nn.Module):
     node_kl_divergence_loss = (node_kl_divergence_loss * 10**n_digits).round() / (10**n_digits)'''
 
     class_kl_divergence_loss.backward(retain_graph=True)
-    #node_kl_divergence_loss.backward(retain_graph=True)
+    node_kl_divergence_loss.backward(retain_graph=True)
     reconstruction_error.backward()
 
     #loss = class_kl_divergence_loss + node_kl_divergence_loss + reconstruction_error
 
     #loss.backward(retain_graph=True)
     
-    #return  reconstruction_error.item(), class_kl_divergence_loss.item() , node_kl_divergence_loss.item()
-    return reconstruction_error.item(), class_kl_divergence_loss.item() , 0
+    return  reconstruction_error.item(), class_kl_divergence_loss.item() , node_kl_divergence_loss.item()
 
 
   def edge_recon(self, z, edge_index, sigmoid=True):
