dc8deee177b4df54ddaade49f6fe0f8cfc1dc167
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-31 20:43:21 +0800

    line graph

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -334,7 +334,7 @@ if __name__ == '__main__':
     #dataset = TUDataset(path, name=DS, pre_transform=torch_geometric.transforms.OneHotDegree(max_degree=88))
     #dataset = TUDataset(path, name=DS, pre_transform=torch_geometric.transforms.Constant(value=1))
     #dataset = TUDataset(path, name=DS, pre_transform=torch_geometric.transforms.LineGraph())
-    dataset = TUDataset(path, name=DS, pre_transform=torch_geometric.transforms.LocalDegreeProfile())
+    dataset = TUDataset(path, name=DS, pre_transform=torch_geometric.transforms.LocalDegreeProfile)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     try:
