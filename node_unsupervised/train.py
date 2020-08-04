b3c1482c3c65798b3ab307227b28a8abc647fbfd
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-04 22:49:56 +0800

    cora init

--- node_unsupervised/train.py
+++ node_unsupervised/train.py
@@ -126,6 +126,51 @@ def train(dataset, verbose=True):
         if verbose:
             print('Epoch: {0}, Loss: {1:0.4f}'.format(epoch, loss))
 
+        model.eval()
+
+        if sparse:
+            adj = sparse_mx_to_torch_sparse_tensor(sp.coo_matrix(adj))
+            diff = sparse_mx_to_torch_sparse_tensor(sp.coo_matrix(diff))
+
+        features = torch.FloatTensor(features[np.newaxis])
+        adj = torch.FloatTensor(adj[np.newaxis])
+        diff = torch.FloatTensor(diff[np.newaxis])
+        features = features.cuda()
+        adj = adj.cuda()
+        diff = diff.cuda()
+
+        embeds = model.get_embeddings(features, adj)
+        train_embs = embeds[0, idx_train]
+        test_embs = embeds[0, idx_test]
+
+        train_lbls = labels[idx_train]
+        test_lbls = labels[idx_test]
+
+        accs = []
+        wd = 0.01 if dataset == 'citeseer' else 0.0
+
+        for _ in range(50):
+            log = LogReg(hid_units, nb_classes)
+            opt = torch.optim.Adam(log.parameters(), lr=1e-2, weight_decay=wd)
+            log.cuda()
+            for _ in range(300):
+                log.train()
+                opt.zero_grad()
+
+                logits = log(train_embs)
+                loss = xent(logits, train_lbls)
+
+                loss.backward()
+                opt.step()
+
+            logits = log(test_embs)
+            preds = torch.argmax(logits, dim=1)
+            acc = torch.sum(preds == test_lbls).float() / test_lbls.shape[0]
+            accs.append(acc * 100)
+
+        accs = torch.stack(accs)
+        print(accs.mean().item(), accs.std().item())
+
         if loss < best:
             best = loss
             best_t = epoch
