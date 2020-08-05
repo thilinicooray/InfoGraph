499e08b25d2ceb805b6bdc35a68b6190558622b9
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-05 11:04:42 +0800

    check mvgrl loss

--- node_unsupervised/train_mvgrl.py
+++ node_unsupervised/train_mvgrl.py
@@ -147,7 +147,7 @@ def train(dataset, verbose=True):
     patience = 20
     lr = 0.001
     l2_coef = 0.0
-    hid_units = 512
+    hid_units = 32
     sparse = False
 
     adj, diff, features, labels, idx_train, idx_val, idx_test = load(dataset)
