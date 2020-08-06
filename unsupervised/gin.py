768068028df86d659e87a6af64f94bf1149744aa
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-06 20:27:23 +0800

    setup 11

--- unsupervised/gin.py
+++ unsupervised/gin.py
@@ -45,7 +45,7 @@ class Encoder(torch.nn.Module):
 
     def forward(self, x, edge_index, batch):
 
-        x = torch.ones((batch.shape[0],5)).double().to('cuda')
+        x = torch.ones((batch.shape[0],5)).to('cuda')
         if x is None:
             x = torch.ones((batch.shape[0], 1)).to(device)
 
