9dc5231227e11af34bde5326ba04a0d5bf66b761
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-07 15:18:44 +0800

    add line graph rep too remove double datatype

--- new_unsupervised_line/dataloader.py
+++ new_unsupervised_line/dataloader.py
@@ -26,4 +26,4 @@ class ConcatDataset(torch.utils.data.Dataset):
         return items_map
 
     def __len__(self):
-        return min(len(d) for d in self.datasets)
+        return min(len(d) for k, d in self.dataset_map.items())
