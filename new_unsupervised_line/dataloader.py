1edd0ec404b59ca9069bed0e678574bd5b557dd5
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-07 16:02:50 +0800

    add line graph rep too remove double datatype

--- new_unsupervised_line/dataloader.py
+++ new_unsupervised_line/dataloader.py
@@ -12,12 +12,41 @@ from torch_geometric.data import Data, Batch
 
 from torch_geometric.transforms import LineGraph
 
+from torch._six import container_abcs, string_classes, int_classes
+
 class Collater(object):
     def __init__(self, follow_batch):
         self.follow_batch = follow_batch
 
     def collate(self, batch):
-        print('batch', batch)
+        elem = batch[0]
+        if isinstance(elem, Data):
+            print('came to data')
+            return Batch.from_data_list(batch, self.follow_batch)
+        elif isinstance(elem, torch.Tensor):
+            print('came to tensor')
+            return default_collate(batch)
+        elif isinstance(elem, float):
+            print('came to float')
+            return torch.tensor(batch, dtype=torch.float)
+        elif isinstance(elem, int_classes):
+            print('came to int')
+            return torch.tensor(batch)
+        elif isinstance(elem, string_classes):
+            print('came to string')
+            return batch
+        elif isinstance(elem, container_abcs.Mapping):
+            print('came to map')
+            return {key: self.collate([d[key] for d in batch]) for key in elem}
+        elif isinstance(elem, tuple) and hasattr(elem, '_fields'):
+            print('came to field')
+            return type(elem)(*(self.collate(s) for s in zip(*batch)))
+        elif isinstance(elem, container_abcs.Sequence):
+            print('came to seq')
+            return [self.collate(s) for s in zip(*batch)]
+
+        raise TypeError('DataLoader found invalid type: {}'.format(type(elem)))
+
 
     def __call__(self, batch):
         return self.collate(batch)
