ff819b6a51967179a89435ace1649efdf91c5f3c
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-08 12:54:44 +0800

    try combining both graphs

--- new_unsupervised/deepinfomax.py
+++ new_unsupervised/deepinfomax.py
@@ -325,8 +325,8 @@ if __name__ == '__main__':
         path_lineg = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data_lineg', DS)
         # kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
 
-        dataset = TUDataset(path, name=DS)
-        dataset_lineg = TUDataset(path_lineg, name=DS, pre_transform=torch_geometric.transforms.LineGraph())
+        dataset = TUDataset(path, name=DS).shuffle()
+        dataset_lineg = TUDataset(path_lineg, name=DS, pre_transform=torch_geometric.transforms.LineGraph()).shuffle()
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         try:
