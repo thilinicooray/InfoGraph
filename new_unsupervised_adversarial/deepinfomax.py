734b90364be8f4993437f0b7e7da830516e239aa
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-15 02:35:37 +0800

    imdb-m aae with current best prev commit also ok

--- new_unsupervised_adversarial/deepinfomax.py
+++ new_unsupervised_adversarial/deepinfomax.py
@@ -374,8 +374,10 @@ if __name__ == '__main__':
                     data.x = torch.ones((data.batch.shape[0], 5)).double().to(device)
 
 
+                model.encoder.zero_grad()
+                model.decoder.zero_grad()
 
-                model.zero_grad()
+                #model.zero_grad()
 
                 z_sample, z_class = model.encoder(data.x, data.edge_index, data.batch)
                 grouped_class = accumulate_group_rep(
@@ -396,7 +398,14 @@ if __name__ == '__main__':
                 ## true prior is random normal (randn)
                 ## this is constraining the Z-projection to be normal!
                 model.encoder.eval()
+                model.class_discriminator.zero_grad()
+                model.class_discriminator.zero_grad()
 
+                '''for p in model.class_discriminator.parameters():
+                    p.requires_grad = True
+
+                for p in model.node_discriminator.parameters():
+                    p.requires_grad = True'''
 
                 z_real_gauss_node = Variable(torch.randn(data.batch.shape[0], args.hidden_dim) * 5.).double().cuda()
                 D_real_gauss_node = model.node_discriminator(z_real_gauss_node)
