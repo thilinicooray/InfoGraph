7c9905cdfab4c019b9396d05f99948ed6c650ae2
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-08-15 19:09:51 +0800

    reddit b init commit

--- new_unsupervised_adversarial/deepinfomax.py
+++ new_unsupervised_adversarial/deepinfomax.py
@@ -311,7 +311,7 @@ if __name__ == '__main__':
         path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data_degree', DS)
         # kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
 
-        dataset = TUDataset(path, name=DS, pre_transform = torch_geometric.transforms.OneHotDegree(max_degree=3061)).shuffle()
+        dataset = TUDataset(path, name=DS, pre_transform = torch_geometric.transforms.OneHotDegree(max_degree=3062)).shuffle()
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         try:
