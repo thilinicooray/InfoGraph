4f5a0b7c02a0c8d3742e81df0b8051c775ae51ef
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-04 16:13:16 +0800

    joint graph node cls

--- Enzymes/evaluate_embedding.py
+++ Enzymes/evaluate_embedding.py
@@ -57,7 +57,7 @@ class LogReg(nn.Module):
 def logistic_classify(x, y):
 
     nb_classes = np.unique(y).shape[0]
-    xent = nn.CrossEntropyLoss(weights=torch.tensor([0.02,0.02,0.5]))
+    xent = nn.CrossEntropyLoss(weight=torch.tensor([0.02,0.02,0.5]))
     hid_units = x.shape[1]
 
     accs = []
