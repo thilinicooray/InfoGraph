a134706e06635567385b04cd284866d910fa679f
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-05 12:46:48 +0800

    trying cmv

---
+++ Enzymes/deepinfomax_cmv.py
@@ -0,0 +1,454 @@
+import os.path as osp
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import json
+import random
+import os
+# from core.encoders import *
+
+from sklearn.metrics import f1_score
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn import preprocessing
+
+import torch_geometric
+from torch_geometric.datasets import TUDataset
+from torch_geometric.data import DataLoader
+from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool
+from torch_geometric.utils import negative_sampling, remove_self_loops, add_self_loops, to_dense_adj, to_dense_batch
+import sys
+import json
+from torch import optim
+
+from cortex_DIM.nn_modules.mi_networks import MIFCNet, MI1x1ConvNet
+from losses import *
+from gin_infograph import Encoder
+from evaluate_embedding import evaluate_embedding, draw_plot
+from model import *
+from utils import imshow_grid, mse_loss, reparameterize, group_wise_reparameterize, accumulate_group_evidence, \
+    accumulate_group_rep, expand_group_rep
+
+from sklearn.linear_model import LogisticRegression
+
+from torch_sparse import coalesce
+import torch_geometric.transforms.GDC.diffusion_matrix_approx as diffusion_matrix_approx
+import torch_geometric.transforms.GDC.sparsify_sparse as sparsify_sparse
+import torch_geometric.transforms.GDC.transition_matrix as transition_matrix
+
+from arguments import arg_parse
+
+class GCNLayer(nn.Module):
+    def __init__(self, in_ft, out_ft, bias=True):
+        super(GCNLayer, self).__init__()
+        self.fc = nn.Linear(in_ft, out_ft, bias=False)
+        self.act = nn.PReLU()
+
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_ft))
+            self.bias.data.fill_(0.0)
+        else:
+            self.register_parameter('bias', None)
+
+        for m in self.modules():
+            self.weights_init(m)
+
+    def weights_init(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight.data)
+            if m.bias is not None:
+                m.bias.data.fill_(0.0)
+
+    def forward(self, feat, adj):
+        feat = self.fc(feat)
+        out = torch.bmm(adj, feat)
+        if self.bias is not None:
+            out += self.bias
+        return self.act(out)
+
+
+class GCN(nn.Module):
+    def __init__(self, in_ft, out_ft, num_layers):
+        super(GCN, self).__init__()
+        n_h = out_ft
+        self.layers = []
+        self.num_layers = num_layers
+        self.layers.append(GCNLayer(in_ft, n_h).cuda())
+        for __ in range(num_layers - 1):
+            self.layers.append(GCNLayer(n_h, n_h).cuda())
+
+    def forward(self, feat, adj):
+        h_1 = self.layers[0](feat, adj)
+        h_1g = torch.sum(h_1, 1)
+        for idx in range(self.num_layers - 1):
+            h_1 = self.layers[idx + 1](h_1, adj)
+            h_1g = torch.cat((h_1g, torch.sum(h_1, 1)), -1)
+        return h_1, h_1g
+
+
+class MLP(nn.Module):
+    def __init__(self, in_ft, out_ft):
+        super(MLP, self).__init__()
+        self.ffn = nn.Sequential(
+            nn.Linear(in_ft, out_ft),
+            nn.PReLU(),
+            nn.Linear(out_ft, out_ft),
+            nn.PReLU(),
+            nn.Linear(out_ft, out_ft),
+            nn.PReLU()
+        )
+        self.linear_shortcut = nn.Linear(in_ft, out_ft)
+
+    def forward(self, x):
+        return self.ffn(x) + self.linear_shortcut(x)
+
+
+class Model(nn.Module):
+    def __init__(self, n_in, n_h, num_layers):
+        super(Model, self).__init__()
+        self.mlp1 = MLP(1 * n_h, n_h)
+        self.mlp2 = MLP(num_layers * n_h, n_h)
+        self.gnn1 = GCN(n_in, n_h, num_layers)
+        self.gnn2 = GCN(n_in, n_h, num_layers)
+
+    def forward(self, adj, diff, feat ):
+        lv1, gv1 = self.gnn1(feat, adj)
+        lv2, gv2 = self.gnn2(feat, diff)
+
+        lv1 = self.mlp1(lv1)
+        lv2 = self.mlp1(lv2)
+
+        gv1 = self.mlp2(gv1)
+        gv2 = self.mlp2(gv2)
+
+        return lv1, gv1, lv2, gv2
+
+    def embed(self, feat, adj, diff):
+        l1, gv1, l2, gv2 = self.forward(adj, diff, feat)
+        return (l1 + l2).detach(),(gv1 + gv2).detach()
+
+    def get_embeddings_whole(self, loader):
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        ret_node = []
+        y_node = []
+        ret_class = []
+        y_class = []
+        with torch.no_grad():
+            for data in loader:
+                data.to(device)
+
+                x_node, x_graph = self.embed(data.edge_index, diff_edge_index, data.x[:,:18])
+
+
+                ret_node.append(x_node.cpu().numpy())
+                node_label_idx = (data.x[:,18:] != 0).nonzero()[:,1]
+                y_node.append(node_label_idx.cpu().numpy())
+                ret_class.append(x_graph.cpu().numpy())
+                y_class.append(data.y.cpu().numpy())
+
+        ret_node = np.concatenate(ret_node, 0)
+        y_node = np.concatenate(y_node, 0)
+        ret_class = np.concatenate(ret_class, 0)
+        y_class = np.concatenate(y_class, 0)
+        return ret_node, y_node, ret_class, y_class
+
+
+# Borrowed from https://github.com/fanyun-sun/InfoGraph
+def get_positive_expectation(p_samples, measure, average=True):
+    """Computes the positive part of a divergence / difference.
+    Args:
+        p_samples: Positive samples.
+        measure: Measure to compute for.
+        average: Average the result over samples.
+    Returns:
+        torch.Tensor
+    """
+    log_2 = np.log(2.)
+
+    if measure == 'GAN':
+        Ep = - F.softplus(-p_samples)
+    elif measure == 'JSD':
+        Ep = log_2 - F.softplus(- p_samples)
+    elif measure == 'X2':
+        Ep = p_samples ** 2
+    elif measure == 'KL':
+        Ep = p_samples + 1.
+    elif measure == 'RKL':
+        Ep = -torch.exp(-p_samples)
+    elif measure == 'DV':
+        Ep = p_samples
+    elif measure == 'H2':
+        Ep = 1. - torch.exp(-p_samples)
+    elif measure == 'W1':
+        Ep = p_samples
+
+    if average:
+        return Ep.mean()
+    else:
+        return Ep
+
+
+# Borrowed from https://github.com/fanyun-sun/InfoGraph
+def get_negative_expectation(q_samples, measure, average=True):
+    """Computes the negative part of a divergence / difference.
+    Args:
+        q_samples: Negative samples.
+        measure: Measure to compute for.
+        average: Average the result over samples.
+    Returns:
+        torch.Tensor
+    """
+    log_2 = np.log(2.)
+
+    if measure == 'GAN':
+        Eq = F.softplus(-q_samples) + q_samples
+    elif measure == 'JSD':
+        Eq = F.softplus(-q_samples) + q_samples - log_2
+    elif measure == 'X2':
+        Eq = -0.5 * ((torch.sqrt(q_samples ** 2) + 1.) ** 2)
+    elif measure == 'KL':
+        Eq = torch.exp(q_samples)
+    elif measure == 'RKL':
+        Eq = q_samples - 1.
+    elif measure == 'H2':
+        Eq = torch.exp(q_samples) - 1.
+    elif measure == 'W1':
+        Eq = q_samples
+
+    if average:
+        return Eq.mean()
+    else:
+        return Eq
+
+
+# Borrowed from https://github.com/fanyun-sun/InfoGraph
+def local_global_loss_(l_enc, g_enc, batch, measure):
+    '''
+    Args:
+        l: Local feature map.
+        g: Global features.
+        measure: Type of f-divergence. For use with mode `fd`
+        mode: Loss mode. Fenchel-dual `fd`, NCE `nce`, or Donsker-Vadadhan `dv`.
+    Returns:
+        torch.Tensor: Loss.
+    '''
+    num_graphs = g_enc.shape[0]
+    num_nodes = l_enc.shape[0]
+    max_nodes = num_nodes // num_graphs
+
+    pos_mask = torch.zeros((num_nodes, num_graphs)).cuda()
+    neg_mask = torch.ones((num_nodes, num_graphs)).cuda()
+    msk = torch.ones((num_nodes, num_graphs)).cuda()
+    for nodeidx, graphidx in enumerate(batch):
+        pos_mask[nodeidx][graphidx] = 1.
+        neg_mask[nodeidx][graphidx] = 0.
+
+
+
+    res = torch.mm(l_enc, g_enc.t())
+
+    E_pos = get_positive_expectation(res * pos_mask, measure, average=False).sum()
+    E_pos = E_pos / num_nodes
+    E_neg = get_negative_expectation(res * neg_mask, measure, average=False).sum()
+    E_neg = E_neg / (num_nodes * (num_graphs - 1))
+    return E_neg - E_pos
+
+
+def global_global_loss_(g1_enc, g2_enc, measure):
+    '''
+    Args:
+        l: Local feature map.
+        g: Global features.
+        measure: Type of f-divergence. For use with mode `fd`
+        mode: Loss mode. Fenchel-dual `fd`, NCE `nce`, or Donsker-Vadadhan `dv`.
+    Returns:
+        torch.Tensor: Loss.
+    '''
+    num_graphs = g1_enc.shape[0]
+
+    pos_mask = torch.zeros((num_graphs, num_graphs)).cuda()
+    neg_mask = torch.ones((num_graphs, num_graphs)).cuda()
+    for graphidx in range(num_graphs):
+        pos_mask[graphidx][graphidx] = 1.
+        neg_mask[graphidx][graphidx] = 0.
+
+    res = torch.mm(g1_enc, g2_enc.t())
+
+    E_pos = get_positive_expectation(res * pos_mask, measure, average=False).sum()
+    E_pos = E_pos / num_graphs
+    E_neg = get_negative_expectation(res * neg_mask, measure, average=False).sum()
+    E_neg = E_neg / (num_graphs * (num_graphs - 1))
+    return E_neg - E_pos
+
+def get_diffusion_edge_index(edge_index, num_nodes):
+    N = num_nodes
+    edge_index = edge_index
+    edge_weight = torch.ones(edge_index.size(1),
+                                 device=edge_index.device)
+
+
+    edge_index, edge_weight = coalesce(edge_index, edge_weight, N, N)
+
+    edge_index, edge_weight = diffusion_matrix_approx(
+            edge_index, edge_weight, N, 'sym',
+        diffusion_kwargs=dict(method='ppr', alpha=0.15))
+    edge_index, edge_weight = sparsify_sparse(
+            edge_index, edge_weight, N, sparsification_kwargs=dict(method='threshold', avg_degree=64))
+
+    edge_index, edge_weight = coalesce(edge_index, edge_weight, N, N)
+    edge_index, edge_weight = transition_matrix(
+        edge_index, edge_weight, N, 'col')
+
+    edge_index = edge_index
+
+    return edge_index
+
+if __name__ == '__main__':
+
+    args = arg_parse()
+
+    #for seed in [32,42,52,62,72]:
+    for seed in [52]:
+
+        #seed = 42
+        #epochs = 37
+        epochs = int(args.num_epochs)
+
+        #for epochs in range(20,41):
+
+        print('seed ', seed, 'epochs ', epochs)
+
+
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        os.environ['PYTHONHASHSEED'] = str(seed)
+
+        print('init seed, seed ', torch.initial_seed(), seed)
+
+        accuracies_node = {'logreg':[], 'svc':[], 'linearsvc':[], 'randomforest':[]}
+        accuracies_class = {'logreg':[], 'svc':[], 'linearsvc':[], 'randomforest':[]}
+
+        losses = {'recon':[], 'node_kl':[], 'class_kl': []}
+
+        warmup_steps = 0
+        #batch_size = 128
+        batch_size = args.batch_size
+        lr = args.lr
+        gen_lr = 1 * lr
+        reg_lr = 0.5 * lr
+
+        EPS = 1e-15
+
+        #lr = 0.000001
+        DS = args.DS
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', DS)
+        # kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
+
+        #dataset = TUDataset(path, name=DS, pre_transform = torch_geometric.transforms.OneHotDegree(max_degree=88)).shuffle()
+        dataset = TUDataset(path, use_node_attr=True, name=DS).shuffle()
+
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        try:
+            dataset_num_features = dataset.num_features - 3
+        except:
+            dataset_num_features = 1
+
+        if not dataset_num_features:
+
+            dataset_num_features = 5
+
+            #dataset_num_features = 5
+            #input_feat = torch.ones((batch_size, 1)).to(device)
+
+        dataloader = DataLoader(dataset, batch_size=batch_size)
+
+
+        model = Model(dataset_num_features, args.hidden_dim, args.num_gc_layers).double().to(device)
+        #encode/decode optimizers
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+
+        print('================')
+        print('num_features: {}'.format(dataset_num_features))
+        print('hidden_dim: {}'.format(args.hidden_dim))
+        print('num_gc_layers: {}'.format(args.num_gc_layers))
+        print('================')
+
+
+        '''model.eval()
+        emb, y = model.get_embeddings(dataloader)
+        res = evaluate_embedding(emb, y)
+        accuracies['logreg'].append(res[0])
+        accuracies['svc'].append(res[1])
+        accuracies['linearsvc'].append(res[2])
+        accuracies['randomforest'].append(res[3])'''
+
+        #model.train()
+        for epoch in range(1, epochs+1):
+            loss_all = 0
+            model.train()
+            for data in dataloader:
+                data = data.to(device)
+
+
+                model.zero_grad()
+
+
+                data.x = data.x.double()
+
+                optimizer.zero_grad()
+
+                diff_edge_index = get_diffusion_edge_index(data.edge_index, data.num_nodes)
+
+                lv1, gv1, lv2, gv2 = model(data.edge_index, diff_edge_index, data.x[:,:18])
+
+
+                loss1 = local_global_loss_(lv1, gv2, data.batch, 'JSD')
+                loss2 = local_global_loss_(lv2, gv1, data.batch, 'JSD')
+                # loss3 = global_global_loss_(gv1, gv2, 'JSD')
+                loss = loss1 + loss2
+
+                loss_all += loss
+
+
+                loss.backward()
+                optimizer.step()
+            print('Epoch {}, Loss {}'.format(epoch, loss_all / len(dataloader)))
+
+            #used during finetune phase
+            if epoch > warmup_steps:
+                model.eval()
+
+                emb_node, y_node, emb_class, y_class = model.get_embeddings_whole(dataloader)
+                print('node classificaion')
+                res = evaluate_embedding(emb_node, y_node)
+                accuracies_node['logreg'].append(res[0])
+                accuracies_node['svc'].append(res[1])
+                accuracies_node['linearsvc'].append(res[2])
+                accuracies_node['randomforest'].append(res[3])
+                print('node ', accuracies_node)
+                '''print('graph classificaion')
+                res = evaluate_embedding(emb_class, y_class)
+                accuracies_class['logreg'].append(res[0])
+                accuracies_class['svc'].append(res[1])
+                accuracies_class['linearsvc'].append(res[2])
+                accuracies_class['randomforest'].append(res[3])
+                print('class ', accuracies_node)'''
+
+
+
+
+
+
+        #draw_plot(y, emb, 'imdb_b_normal.png')
+
+        '''with open('unsupervised.log', 'a+') as f:
+            s = json.dumps(accuracies)
+            f.write('{},{},{},{},{},{}\n'.format(args.DS, args.num_gc_layers, epochs, log_interval, lr, s))'''
