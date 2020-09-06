423e38421730d328ae4c8036de9c35ff83b252f8
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-06 19:51:14 +0800

    gvae

--- Enzymes/gin_gvae.py
+++ Enzymes/gin_gvae.py
@@ -90,7 +90,6 @@ class Encoder(torch.nn.Module):
         node_latent_space_mu = self.bns[j](torch.tanh(self.convs[j](out, edge_index)))
         node_latent_space_logvar = self.bns[j+1](torch.tanh(self.convs[j+1](out, edge_index)))
 
-
         '''node_latent_space_mu = F.relu(self.node_mu(x))
         node_latent_space_logvar = F.relu(self.node_logvar(x))
 
@@ -107,7 +106,7 @@ class Decoder(torch.nn.Module):
         super(Decoder, self).__init__()
 
         self.linear_model = torch.nn.Sequential(OrderedDict([
-            ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=node_dim, bias=True)),
+            ('linear_1', torch.nn.Linear(in_features=node_dim , out_features=node_dim, bias=True)),
             ('relu_1', ReLU()),
 
             ('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
