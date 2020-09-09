89b8c69870e26f0d12a61354ab73644b0e452720
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-09 13:09:58 +0800

    mutag run

--- Mutag/deepinfomax_vae.py
+++ Mutag/deepinfomax_vae.py
@@ -416,7 +416,7 @@ if __name__ == '__main__':
                 data = data.to(device)
 
                 optimizer.zero_grad()
-                recon_loss, kl_class, kl_node = model(data.double(), data.edge_index, data.batch, data.num_graphs)
+                recon_loss, kl_class, kl_node = model(data.x.double(), data.edge_index, data.batch, data.num_graphs)
                 #current_loss = model(data.x[:,:18].double(), data.edge_index, data.batch, data.num_graphs)
                 recon_loss_all += recon_loss
                 kl_class_loss_all += kl_class
