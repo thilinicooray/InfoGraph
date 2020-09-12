f46478e3e4c2046fc9150e7db3285cdaab3f8aa5
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-12 22:27:36 +0800

    dgi

--- Cora/dgi.py
+++ Cora/dgi.py
@@ -22,6 +22,8 @@ import sys
 import json
 from torch import optim
 
+from torch_geometric.nn import GINConv, global_add_pool, GCNConv
+
 from cortex_DIM.nn_modules.mi_networks import MIFCNet, MI1x1ConvNet
 from losses import *
 from gcn_mlaae import Encoder, Decoder
@@ -34,182 +36,41 @@ from sklearn.linear_model import LogisticRegression
 
 from arguments import arg_parse
 
-class D_net_gauss(nn.Module):
-    def __init__(self,N,z_dim):
-        super(D_net_gauss, self).__init__()
-        self.lin1 = nn.Linear(z_dim, N)
-        self.lin2 = nn.Linear(N, N)
-        self.lin3 = nn.Linear(N, 1)
-    def forward(self, x):
-        x = self.lin1(x)
-        '''x = F.relu(x)
-        x = F.dropout(self.lin2(x), p=0.0, training=self.training)'''
-        x = F.relu(x)
-        return torch.sigmoid(self.lin3(x))
+import torch
+import torch.nn as nn
+from layers import GCN, AvgReadout, Discriminator
 
-class GcnInfomax(nn.Module):
-    def __init__(self, hidden_dim, num_gc_layers, alpha=0.5, beta=1., gamma=.1):
-        super(GcnInfomax, self).__init__()
+class DGI(nn.Module):
+    def __init__(self, n_in, n_h, activation):
+        super(DGI, self).__init__()
+        self.gcn = GCNConv(n_in, n_h)
+        self.read = AvgReadout()
+        self.act = PReLU()
 
-        self.alpha = alpha
-        self.beta = beta
-        self.gamma = gamma
-        self.prior = args.prior
+        self.sigm = nn.Sigmoid()
 
-        self.encoder = Encoder(dataset_num_features, hidden_dim, num_gc_layers)
-        self.decoder = Decoder(hidden_dim, hidden_dim, dataset_num_features)
-        self.node_discriminator = D_net_gauss(hidden_dim, hidden_dim)
-        self.class_discriminator = D_net_gauss(hidden_dim, hidden_dim)
+        self.disc = Discriminator(n_h)
 
+    def forward(self, x, x_permute, edge_index):
+        h_1 = self.act(self.gcn(x, edge_index))
 
+        c = self.read(h_1, None)
+        c = self.sigm(c)
 
-        self.init_emb()
+        h_2 = self.act(self.gcn(x_permute, edge_index))
 
-    def init_emb(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                torch.nn.init.xavier_uniform_(m.weight.data)
-                if m.bias is not None:
-                    m.bias.data.fill_(0.0)
+        ret = self.disc(c, h_1, h_2)
+
+        return ret
+
+    # Detach the return variables
+    def embed(self, seq, adj, sparse, msk):
+        h_1 = self.gcn(seq, adj, sparse)
+        c = self.read(h_1, msk)
+
+        return h_1.detach(), c.detach()
 
 
-    def forward(self, x, edge_index, batch, num_graphs):
-
-
-
-        n_nodes = x.size(0)
-
-        # batch_size = data.num_graphs
-
-        node_mu, node_logvar = self.encoder(x, edge_index, batch)
-
-
-
-
-
-
-
-
-
-        # kl-divergence error for style latent space
-        '''node_kl_divergence_loss = torch.mean(
-            - 0.5 * torch.sum(1 + node_logvar - node_mu.pow(2) - node_logvar.exp())
-        )'''
-
-        node_kl_divergence_loss = -0.5 * torch.mean(torch.sum(
-            1 + 2 * node_logvar - node_mu.pow(2) - node_logvar.exp().pow(2), 1))
-
-
-        node_kl_divergence_loss = node_kl_divergence_loss
-
-
-        # kl-divergence error for class latent space
-        '''class_kl_divergence_loss = torch.mean(
-            - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
-        )'''
-
-        #print('class kl unwei ', class_kl_divergence_loss)
-        #print('class kl wei ', class_kl_divergence_loss)
-
-
-        # reconstruct samples
-        """
-        sampling from group mu and logvar for each graph in mini-batch differently makes
-        the decoder consider class latent embeddings as random noise and ignore them 
-        """
-        node_latent_embeddings = reparameterize(training=True, mu=node_mu, logvar=node_logvar)
-
-
-        reconstructed_node = self.decoder(node_latent_embeddings)
-
-        #reconstruction_error =  mse_loss(reconstructed_node, x) * num_graphs
-        reconstruction_error = self.recon_loss1(reconstructed_node, edge_index, batch)
-
-
-        #class_kl_divergence_loss.backward(retain_graph=True)
-        #node_kl_divergence_loss.backward(retain_graph=True)
-        #reconstruction_error.backward()
-
-        loss =   node_kl_divergence_loss + reconstruction_error
-
-        loss.backward()
-
-
-        return  reconstruction_error.item(), 0 , node_kl_divergence_loss.item()
-
-
-    def edge_recon(self, z, edge_index, sigmoid=True):
-        r"""Decodes the latent variables :obj:`z` into edge probabilities for
-        the given node-pairs :obj:`edge_index`.
-
-        Args:
-            z (Tensor): The latent space :math:`\mathbf{Z}`.
-            sigmoid (bool, optional): If set to :obj:`False`, does not apply
-                the logistic sigmoid function to the output.
-                (default: :obj:`True`)
-        """
-        value = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=1)
-        return torch.sigmoid(value) if sigmoid else value
-
-
-
-    def recon_loss1(self, z, edge_index):
-
-        EPS = 1e-15
-        MAX_LOGSTD = 10
-        r"""Given latent variables :obj:`z`, computes the binary cross
-        entropy loss for positive edges :obj:`pos_edge_index` and negative
-        sampled edges.
-  
-        Args:
-            z (Tensor): The latent space :math:`\mathbf{Z}`.
-            pos_edge_index (LongTensor): The positive edges to train against.
-        """
-
-        #org_adj = to_dense_adj(edge_index, batch)
-        pos_weight = float(z.size(0) * z.size(0) - edge_index.size(0)) / edge_index.size(0)
-        norm = z.size(0) * z.size(0) / float((z.size(0) * z.size(0) - edge_index.size(0)) * 2)
-
-
-
-        recon_adj = self.edge_recon(z, edge_index)
-
-
-        pos_loss = -torch.log(
-            recon_adj + EPS).mean()
-
-        # Do not include self-loops in negative samples
-        pos_edge_index, _ = remove_self_loops(edge_index)
-        pos_edge_index, _ = add_self_loops(pos_edge_index)
-
-        neg_edge_index = negative_sampling(pos_edge_index, z.size(0)) #random thingggg
-        neg_loss = -torch.log(1 -
-                              self.edge_recon(z, neg_edge_index) +
-                              EPS).mean()
-
-        return norm*(pos_loss*pos_weight + neg_loss)
-
-    def get_embeddings(self, data):
-
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        ret = []
-        y = []
-        with torch.no_grad():
-
-            data.to(device)
-            x, edge_index = data.x, data.edge_index
-
-
-            node_latent, graph_latent = self.encoder(x.double(), edge_index)
-
-        train_emb = node_latent[data.train_mask].cpu().numpy()
-        train_y = data.y[data.train_mask].cpu().numpy()
-        val_emb = node_latent[data.val_mask].cpu().numpy()
-        val_y = data.y[data.val_mask].cpu().numpy()
-        test_emb = node_latent[data.test_mask].cpu().numpy()
-        test_y = data.y[data.test_mask].cpu().numpy()
-
-        return train_emb, train_y, val_emb, val_y,test_emb, test_y
 
 def test(train_z, train_y, val_z, val_y,test_z, test_y,  solver='lbfgs',
          multi_class='ovr', *args, **kwargs):
@@ -333,11 +194,6 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
         try:
             dataset_num_features = data.x.size(-1)
         except:
@@ -360,17 +216,10 @@ if __name__ == '__main__':
         nb_classes = np.unique(data.y.cpu().numpy()).shape[0]
 
 
-        model = GcnInfomax(args.hidden_dim, args.num_gc_layers).double().to(device)
+        model = model = DGI(dataset_num_features, args.hidden_dim).double().to(device)
         #encode/decode optimizers
         #optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        optim_P = torch.optim.Adam(model.decoder.parameters(), lr=gen_lr)
-        optim_Q_enc = torch.optim.Adam(model.encoder.parameters(), lr=gen_lr)
-        #regularizing optimizers
-        optim_Q_gen = torch.optim.Adam(model.encoder.parameters(), lr=reg_lr)
-        optim_D = torch.optim.Adam([
-            {'params': model.node_discriminator.parameters()},
-            {'params': model.class_discriminator.parameters()}
-        ], lr=reg_lr)
+        optimiser = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0)
 
 
 
@@ -396,6 +245,7 @@ if __name__ == '__main__':
         best_val = 0
 
         xent = nn.CrossEntropyLoss()
+        b_xent = nn.BCEWithLogitsLoss()
 
         #model.train()
         for epoch in range(1, epochs+1):
@@ -406,117 +256,25 @@ if __name__ == '__main__':
             model.train()
 
 
-            model.zero_grad()
+            idx = np.random.permutation(data.x.size(0))
+            print('idx ', idx)
+            shuf_fts = torch.from_numpy(data.x.cpu().numpy()[idx, :]).double()
 
-            z_sample, z_class = model.encoder(data.x.double(), data.edge_index)
-            grouped_class = accumulate_group_rep(
-                z_class
-            )
+            lbl_1 = torch.ones(batch_size, data.x.size(0))
+            lbl_2 = torch.zeros(batch_size, data.x.size(0))
+            lbl = torch.cat((lbl_1, lbl_2), 1)
 
+            if torch.cuda.is_available():
+                shuf_fts = shuf_fts.cuda()
+                lbl = lbl.cuda()
 
-            #encode to z
-            X_sample = model.decoder(z_sample, grouped_class) #decode to X reconstruction
-            recon_loss = model.recon_loss1(X_sample, data.edge_index) + mse_loss(X_sample, data.x.double())
-            recon_loss_all += recon_loss.item()
+            logits = model(data.x.double(), shuf_fts, data.edge_index)
 
-            recon_loss.backward()
-            optim_P.step()
-            optim_Q_enc.step()
+            loss = b_xent(logits, lbl)
 
-            # Discriminator
-            ## true prior is random normal (randn)
-            ## this is constraining the Z-projection to be normal!
-            model.encoder.eval()
-            #model.class_discriminator.zero_grad()
-            #model.class_discriminator.zero_grad()
+            print('Current Loss:', loss.item())
 
-
-            z_real_gauss_node = Variable(torch.randn(data.x.shape[0], args.hidden_dim) ).double().cuda()
-            D_real_gauss_node = model.node_discriminator(z_real_gauss_node)
-
-            z_real_gauss_class = Variable(torch.randn(1, args.hidden_dim)).double().cuda()
-
-            z_real_gauss_class_exp = expand_group_rep(z_real_gauss_class,  data.x.shape[0], args.hidden_dim)
-
-
-            D_real_gauss_class = model.class_discriminator(z_real_gauss_class_exp)
-
-            z_fake_gauss_node, z_fake_gauss_class = model.encoder(data.x.double(), data.edge_index)
-
-            grouped_z_fake_gauss_class = accumulate_group_rep(
-                z_fake_gauss_class
-            )
-
-            D_fake_gauss_node = model.node_discriminator(z_fake_gauss_node)
-            D_fake_gauss_class = model.class_discriminator(grouped_z_fake_gauss_class)
-
-
-
-            D_loss_node = -torch.mean(torch.log(D_real_gauss_node + EPS) + torch.log(1 - D_fake_gauss_node + EPS))
-            D_loss_class = -torch.mean(torch.log(D_real_gauss_class + EPS) + torch.log(1 - D_fake_gauss_class + EPS))
-
-
-
-
-            D_loss = D_loss_node + D_loss_class
-
-            disc_loss_all += D_loss.item()
-
-            D_loss.backward()
-            optim_D.step()
-
-            # Generator
-            model.encoder.train()
-
-            z_fake_gauss_node, z_fake_gauss_class = model.encoder(data.x.double(), data.edge_index)
-
-            grouped_z_fake_gauss_class = accumulate_group_rep(
-                z_fake_gauss_class
-            )
-
-            D_fake_gauss_node = model.node_discriminator(z_fake_gauss_node)
-            D_fake_gauss_class = model.class_discriminator(grouped_z_fake_gauss_class)
-
-
-
-            G_loss_node = -torch.mean(torch.log(D_fake_gauss_node + EPS))
-            G_loss_class = -torch.mean(torch.log(D_fake_gauss_class + EPS))
-
-            G_loss = G_loss_node + G_loss_class
-            #G_loss = G_loss_node
-
-            gen_loss_all += G_loss.item()
-
-            optim_Q_gen.zero_grad()
-            G_loss.backward()
-            optim_Q_gen.step()
-
-            losses['recon'].append(recon_loss_all)
-            losses['gen'].append(gen_loss_all)
-            losses['disc'].append(disc_loss_all)
-
-
-
-            print('Epoch {}, Recon Loss {} Gen Loss {} Disc Loss {}'.format(epoch, recon_loss_all ,
-                                                                            gen_loss_all , disc_loss_all ))
-
-            #used during finetune phase
-            '''if epoch % log_interval == 0:
-                model.eval()
-                #first train logistic regressor
-                #put it eval mode
-                #get eval F1
-                #get test F1
-
-
-
-
-                train_emb, train_y = model.get_embeddings(train_dataloader)
-                val_emb, val_y = model.get_embeddings(val_dataloader)
-                test_emb, test_y = model.get_embeddings(test_dataloader)
-                val_f1, test_f1 = test(train_emb, train_y, val_emb, val_y,test_emb, test_y)
-
-                print('val and test micro F1', val_f1, test_f1)'''
+            losses['recon'].append(loss.item())
 
             print('all losses ', losses)
 
