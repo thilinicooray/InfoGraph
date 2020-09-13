16c68ab30073e24d96ae1a8c48ccc5dbecd8e48e
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-13 17:13:39 +0800

    try triplet loss

--- Mutag/deepinfomax_vae_contra.py
+++ Mutag/deepinfomax_vae_contra.py
@@ -157,6 +157,16 @@ class GcnInfomax(nn.Module):
 
         #kl_div_between_nodegraph = self.compute_two_gaussian_loss(node_mu, node_logvar, grouped_mu, grouped_logvar)
 
+        triplet_loss = nn.TripletMarginLoss(margin=1.0, p=2)
+
+        nodelevel_graph_latent_embeddings = reparameterize(training=True, mu=class_mu, logvar=class_logvar)
+
+        trip_loss = triplet_loss(class_latent_embeddings, nodelevel_graph_latent_embeddings, node_latent_embeddings)
+
+        print('trip loss ', trip_loss.item())
+
+
+
 
         loss =  class_kl_divergence_loss + node_kl_divergence_loss + reconstruction_error
 
