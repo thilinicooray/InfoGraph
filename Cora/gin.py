fcf84e6fa16b8abf63364a056eb0756efd4591c1
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-14 13:14:02 +0800

    mlvae for cora

--- Cora/gin.py
+++ Cora/gin.py
@@ -101,7 +101,7 @@ class Decoder(torch.nn.Module):
             ('relu_1', ReLU()),
 
             ('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
-            ('relu_final', Tanh())
+            ('relu_final', Sigmoid())
         ]))
 
     def forward(self, node_latent_space, class_latent_space):
