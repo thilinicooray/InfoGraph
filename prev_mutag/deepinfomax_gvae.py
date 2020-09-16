db7f6331da88f3d58c6be39c480637817e87fa8b
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-16 16:45:37 +0800

    mutag gvae

--- prev_mutag/deepinfomax_gvae.py
+++ prev_mutag/deepinfomax_gvae.py
@@ -78,6 +78,8 @@ class GcnInfomax(nn.Module):
         """
         node_latent_embeddings = reparameterize(training=True, mu=node_mu, logvar=node_logvar)
 
+        print('latent size ', node_latent_embeddings.size())
+
         #need to reduce ml between node and class latents
         '''measure='JSD'
         mi_loss = local_global_loss_disen(node_latent_embeddings, class_latent_embeddings, edge_index, batch, measure)
