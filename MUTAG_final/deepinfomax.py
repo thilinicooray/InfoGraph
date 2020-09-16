b660bf9b6beae1aec48c7c01091c1bf205b1b977
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-17 07:54:56 +0800

    mutag and ptc

--- MUTAG_final/deepinfomax.py
+++ MUTAG_final/deepinfomax.py
@@ -133,10 +133,10 @@ if __name__ == '__main__':
     
     args = arg_parse()
 
-    seeds = [32,42,52,62,72]
+    #seeds = [32,42,52,62,72]
 
     #seed = 42
-    #seeds = [123,132,213,231,312,321]
+    seeds = [123,132,213,231,312,321]
     epochs_list = [20, 30, 40, 50]
     node_ratio = [0.25, 0.5, 0.75]
     for seed in seeds:
