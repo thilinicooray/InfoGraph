ae2685e115152cc13a22b2dd0b9949247f7df639
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-19 02:22:02 +0800

    mutag eval

--- MUTAG_final_eval/eval.py
+++ MUTAG_final_eval/eval.py
@@ -127,8 +127,8 @@ class GcnInfomax(nn.Module):
                     training=False, mu=grouped_mu, logvar=grouped_logvar, labels_batch=batch, cuda=True
                 )
 
-                sim_node = torch.sigmoid(torch.matmul(entangled.t(), entangled)).cpu().numpy()
-                graph_node = torch.sigmoid(torch.matmul(entangled.t(), accumulated_class_latent_embeddings)).cpu().numpy()
+                sim_node = torch.sigmoid(torch.matmul(node_mu.t(), node_mu)).cpu().numpy()
+                graph_node = torch.sigmoid(torch.matmul(class_mu.t(), class_mu)).cpu().numpy()
                 savetxt('node.csv', sim_node, delimiter=',')
                 savetxt('graph.csv', graph_node, delimiter=',')
 
