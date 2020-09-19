75301725b25b84cf90741367bab91f3929d4deae
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-19 10:53:39 +0800

    mutag eval

--- MUTAG_final_eval/eval.py
+++ MUTAG_final_eval/eval.py
@@ -118,7 +118,7 @@ class GcnInfomax(nn.Module):
 
                 node_mu, node_logvar, class_mu, class_logvar, entangled = self.encoder(x, edge_index, batch)
 
-                print('class mu ', class_mu[:5,:5])
+                print('class mu ', torch.softmax(class_mu, dim=-1)[:5,:5])
 
                 node_latent_embeddings = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
 
