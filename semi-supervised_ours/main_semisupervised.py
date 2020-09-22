0475910930d6d73c2ae9f0fe22d058b7d0e5a1bd
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-22 20:23:58 +0800

    new semi out

--- semi-supervised_ours/main_semisupervised.py
+++ semi-supervised_ours/main_semisupervised.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
 
     target = args.target
     dim = 64
-    epochs = 500
+    epochs = 200
     batch_size = 20
     lamda = args.lamda
     use_unsup_loss = args.use_unsup_loss
