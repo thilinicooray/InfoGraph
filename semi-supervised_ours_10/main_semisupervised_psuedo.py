0631bf2ced4e6ac9a8ec63bdba22e55c835bc609
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-24 18:48:42 +0800

    new semi out target 1

--- semi-supervised_ours_10/main_semisupervised_psuedo.py
+++ semi-supervised_ours_10/main_semisupervised_psuedo.py
@@ -70,6 +70,8 @@ def train(epoch, use_unsup_loss):
 
         sup_loss = F.mse_loss(node_graph_pred, data.y)
 
+        print(' sup loss', sup_loss.item())
+
         sup_loss.backward()
 
         optimizer_teacher.step()
@@ -216,6 +218,10 @@ if __name__ == '__main__':
 
         print('Epoch: {:03d}, LR: {:7f} Validation MAE: {:.7f}, '
               'Test MAE: {:.7f},'.format(epoch, lr, val_error, test_error))
+
+
+        print('all sup losses ', sup_losses)
+        print('all unsup losses ', unsup_losses)
 
 
 
