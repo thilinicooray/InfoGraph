24a41b850c8347d7e5e7937182db48ad0d340862
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-27 00:06:53 +0800

    new semi out target 1

--- semi_supervised_ours_multitarget/main.py
+++ semi_supervised_ours_multitarget/main.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
     target = args.target
     dim = 32
     epochs = 200
-    batch_size = 10
+    batch_size = 20
     lamda = args.lamda
     use_unsup_loss = args.use_unsup_loss
     separate_encoder = args.separate_encoder
