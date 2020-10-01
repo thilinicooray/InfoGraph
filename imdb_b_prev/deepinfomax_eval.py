bcd4fa6b9288d8342921c0073f667ca86ac3e24e
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-10-01 10:12:06 +0800

    semi

--- imdb_b_prev/deepinfomax_eval.py
+++ imdb_b_prev/deepinfomax_eval.py
@@ -83,7 +83,7 @@ class GcnInfomax(nn.Module):
             1 + 2 * node_logvar - node_mu.pow(2) - node_logvar.exp().pow(2), 1))
 
 
-        node_kl_divergence_loss = 10000*node_kl_divergence_loss
+        node_kl_divergence_loss = 100*node_kl_divergence_loss
 
 
         # kl-divergence error for class latent space
@@ -94,7 +94,7 @@ class GcnInfomax(nn.Module):
             1 + 2 * grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp().pow(2), 1))
 
         #print('class kl unwei ', class_kl_divergence_loss)
-        class_kl_divergence_loss = 10000*class_kl_divergence_loss
+        class_kl_divergence_loss = 100*class_kl_divergence_loss
         #print('class kl wei ', class_kl_divergence_loss)
 
 
