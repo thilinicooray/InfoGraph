823a78863795fc99509906997f96985d09ce5ff2
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-10-27 17:24:10 +0800

    ppi train with gate

--- PPI/gin.py
+++ PPI/gin.py
@@ -118,7 +118,8 @@ class Decoder(torch.nn.Module):
         ]))
 
     def forward(self, node_latent_space):
-        x = torch.cat((node_latent_space), dim=1)
+        #x = torch.cat((node_latent_space), dim=1)
+        x = node_latent_space
 
         #x = torch.softmax(self.linear_model(x), dim=-1)
         x = self.linear_model(x)
