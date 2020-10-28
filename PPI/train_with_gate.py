f862bcf6b477d52e00f1207fad5c69dbf625015a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-10-28 22:58:13 +0800

    ppi train with gate

--- PPI/train_with_gate.py
+++ PPI/train_with_gate.py
@@ -58,7 +58,7 @@ class GcnInfomax(nn.Module):
         self.prior = args.prior
 
         self.encoder = Encoder(dataset_num_features, hidden_dim, num_gc_layers, node_dim, class_dim)
-        self.decoder = Decoder(hidden_dim, hidden_dim, hidden_dim)
+        self.decoder = Decoder(hidden_dim, hidden_dim, dataset_num_features)
         self.node_discriminator = D_net_gauss(hidden_dim, hidden_dim)
         self.class_discriminator = D_net_gauss(hidden_dim, hidden_dim)
         self.lamda = lamda
