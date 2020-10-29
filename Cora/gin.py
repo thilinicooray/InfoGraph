501e2a9f2896c43fec8ed38fdb80f51065033467
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-10-29 18:03:18 +0800

    cora train with gate

--- Cora/gin.py
+++ Cora/gin.py
@@ -102,15 +102,16 @@ class Decoder(torch.nn.Module):
         super(Decoder, self).__init__()
 
         self.linear_model = torch.nn.Sequential(OrderedDict([
-            ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=feat_size, bias=True)),
+            ('linear_1', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
             ('relu_1', Tanh()),
 
             #('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
             #('relu_final', Sigmoid())
         ]))
 
-    def forward(self, node_latent_space, class_latent_space):
-        x = torch.cat((node_latent_space, class_latent_space), dim=1)
+    def forward(self, node_latent_space):
+        #x = torch.cat((node_latent_space, class_latent_space), dim=1)
+        x = node_latent_space
 
         #x = torch.softmax(self.linear_model(x), dim=-1)
         x = self.linear_model(x)
