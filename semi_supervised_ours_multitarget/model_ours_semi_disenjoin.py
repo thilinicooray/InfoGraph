1c53973035c754501d06d9159a249105151ad9dd
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-05 18:48:06 +0800

    semi sup joint

--- semi_supervised_ours_multitarget/model_ours_semi_disenjoin.py
+++ semi_supervised_ours_multitarget/model_ours_semi_disenjoin.py
@@ -181,7 +181,11 @@ class Net(torch.nn.Module):
 
         self.ff1 = FF(2*dim, dim)
         self.ff2 = FF(2*dim, dim)
-        self.ff3 = torch.nn.Linear(4*dim, dim)
+        self.ff3 = nn.Sequential(
+            nn.Linear(4*dim, dim),
+            nn.ReLU(),
+            nn.Linear(dim, dim),
+        )
 
         self.set2set_disennodes = Set2Set(dim, processing_steps=3)
 
