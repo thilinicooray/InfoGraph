e5e336d4764069d8df49aa9afab7bb578db783ef
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-10 12:55:19 +0800

    semi sup joint

--- semi_sup_org_new/model_ours.py
+++ semi_sup_org_new/model_ours.py
@@ -176,7 +176,7 @@ class Net(torch.nn.Module):
             1 + 2 * node_logvar - node_mu.pow(2) - node_logvar.exp().pow(2), 1))
 
 
-        node_kl_divergence_loss = 10*node_kl_divergence_loss
+        node_kl_divergence_loss = 1000*node_kl_divergence_loss
 
 
         # kl-divergence error for class latent space
@@ -187,7 +187,7 @@ class Net(torch.nn.Module):
             1 + 2 * grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp().pow(2), 1))
 
         #print('class kl unwei ', class_kl_divergence_loss)
-        class_kl_divergence_loss = 10*class_kl_divergence_loss
+        class_kl_divergence_loss = 1000*class_kl_divergence_loss
         #print('class kl wei ', class_kl_divergence_loss)
 
 
