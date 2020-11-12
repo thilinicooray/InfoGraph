1f1364fcb3022ce1de76b0c4d54017b9f17140cb
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-12 13:16:34 +0800

    gvae run

--- ptc_mr_prev/deepinfomax_gvae_nonodedata.py
+++ ptc_mr_prev/deepinfomax_gvae_nonodedata.py
@@ -99,7 +99,7 @@ class GcnInfomax(nn.Module):
         reconstructed_node = self.decoder(node_latent_embeddings)
 
         #reconstruction_error =  mse_loss(reconstructed_node, x) * num_graphs
-        reconstruction_error = 1e-7*self.recon_loss1(reconstructed_node, edge_index, batch)
+        reconstruction_error = 1e-5*self.recon_loss1(reconstructed_node, edge_index, batch)
 
 
         #class_kl_divergence_loss.backward(retain_graph=True)
