1a92db3fa3d45073cf49c1a52d65aa08a962ce44
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-12 11:42:14 +0800

    gvae run

--- ptc_mr_prev/deepinfomax_gvae.py
+++ ptc_mr_prev/deepinfomax_gvae.py
@@ -105,9 +105,20 @@ class GcnInfomax(nn.Module):
                     x = torch.ones((batch.shape[0],1)).to(device)
                 node_mu, node_logvar = self.encoder(x, edge_index, batch)
 
-                node_latent_embeddings = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
+                #node_latent_embeddings = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
 
-                class_emb = global_add_pool(node_latent_embeddings, batch)
+                #class_emb = global_add_pool(node_latent_embeddings, batch)
+
+                grouped_node_mu, grouped_node_logvar = accumulate_group_evidence(
+                    node_mu.data, node_logvar.data, batch, True
+                )
+
+                accumulated_node_latent_embeddings = group_wise_reparameterize(
+                    training=False, mu=grouped_node_mu, logvar=grouped_node_logvar, labels_batch=batch, cuda=True
+                )
+
+                class_emb = global_mean_pool(accumulated_node_latent_embeddings, batch)
+
                 ret.append(class_emb.cpu().numpy())
                 y.append(data.y.cpu().numpy())
         ret = np.concatenate(ret, 0)
@@ -120,79 +131,83 @@ if __name__ == '__main__':
 
 
     seed = 62
-    epochs = 30
+    #epochs = 20
 
-    print('seed ', seed, 'epochs ', epochs)
+    epochs_list = [20, 30, 40, 50]
 
+    for epochs in epochs_list:
 
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-
-    print('init seed, seed ', torch.initial_seed(), seed)
-
-    accuracies = {'logreg':[], 'svc':[], 'linearsvc':[], 'randomforest':[]}
-
-    log_interval = 1
-    batch_size = 128
-    lr = args.lr
-    DS = args.DS
-    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', DS)
-    # kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
-
-    dataset = TUDataset(path, name=DS).shuffle()
-    try:
-        dataset_num_features = dataset.num_features
-    except:
-        dataset_num_features = 1
-
-    if not dataset_num_features:
-        dataset_num_features = 1
-
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GcnInfomax(args.hidden_dim, args.num_gc_layers).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-
-    print('================')
-    print('lr: {}'.format(lr))
-    print('num_features: {}'.format(dataset_num_features))
-    print('hidden_dim: {}'.format(args.hidden_dim))
-    print('num_gc_layers: {}'.format(args.num_gc_layers))
-    print('================')
+        print('seed ', seed, 'epochs ', epochs)
 
 
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
 
-    model.train()
-    for epoch in range(1, epochs+1):
-        recon_loss_all = 0
-        kl_class_loss_all = 0
-        kl_node_loss_all = 0
-        mi_loss_all = 0
-        #model.train()
-        for data in dataloader:
-            data = data.to(device)
-            optimizer.zero_grad()
-            recon_loss, kl_class, kl_node = model(data.x, data.edge_index, data.batch, data.num_graphs)
-            recon_loss_all += recon_loss
-            kl_class_loss_all += kl_class
-            kl_node_loss_all += kl_node
-            optimizer.step()
+        print('init seed, seed ', torch.initial_seed(), seed)
 
-        print('Epoch {}, Recon Loss {} KL class Loss {} KL node Loss {}'.format(epoch, recon_loss_all / len(dataloader),
-                                                                                kl_class_loss_all / len(dataloader), kl_node_loss_all / len(dataloader)))
+        accuracies = {'logreg':[], 'svc':[], 'linearsvc':[], 'randomforest':[]}
+
+        log_interval = 1
+        batch_size = 128
+        lr = args.lr
+        DS = args.DS
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', DS)
+        # kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
+
+        dataset = TUDataset(path, name=DS).shuffle()
+        try:
+            dataset_num_features = dataset.num_features
+        except:
+            dataset_num_features = 1
+
+        if not dataset_num_features:
+            dataset_num_features = 1
+
+        dataloader = DataLoader(dataset, batch_size=batch_size)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = GcnInfomax(args.hidden_dim, args.num_gc_layers).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+
+        print('================')
+        print('lr: {}'.format(lr))
+        print('num_features: {}'.format(dataset_num_features))
+        print('hidden_dim: {}'.format(args.hidden_dim))
+        print('num_gc_layers: {}'.format(args.num_gc_layers))
+        print('================')
 
 
-    model.eval()
-    emb, y = model.get_embeddings(dataloader)
-    res = evaluate_embedding(emb, y)
-    accuracies['logreg'].append(res[0])
-    accuracies['svc'].append(res[1])
-    accuracies['linearsvc'].append(res[2])
-    accuracies['randomforest'].append(res[3])
-    print(accuracies)
+
+        model.train()
+        for epoch in range(1, epochs+1):
+            recon_loss_all = 0
+            kl_class_loss_all = 0
+            kl_node_loss_all = 0
+            mi_loss_all = 0
+            #model.train()
+            for data in dataloader:
+                data = data.to(device)
+                optimizer.zero_grad()
+                recon_loss, kl_class, kl_node = model(data.x, data.edge_index, data.batch, data.num_graphs)
+                recon_loss_all += recon_loss
+                kl_class_loss_all += kl_class
+                kl_node_loss_all += kl_node
+                optimizer.step()
+
+            print('Epoch {}, Recon Loss {} KL class Loss {} KL node Loss {}'.format(epoch, recon_loss_all / len(dataloader),
+                                                                                    kl_class_loss_all / len(dataloader), kl_node_loss_all / len(dataloader)))
+
+
+        model.eval()
+        emb, y = model.get_embeddings(dataloader)
+        res = evaluate_embedding(emb, y)
+        accuracies['logreg'].append(res[0])
+        accuracies['svc'].append(res[1])
+        accuracies['linearsvc'].append(res[2])
+        accuracies['randomforest'].append(res[3])
+        print(accuracies)
 
 
