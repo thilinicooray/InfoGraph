10047997c82dd0ef70fcaf9733004cb242cb9918
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-16 00:44:04 +0800

    synthetic data

--- synthetic/main_eval.py
+++ synthetic/main_eval.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_loader = DataLoader(train_dataset, batch_size=512)
-    test_loader = DataLoader(test_dataset, batch_size=512)
+    test_loader = DataLoader(test_dataset, batch_size=1)
 
     lr = args.lr
     epochs = 50
