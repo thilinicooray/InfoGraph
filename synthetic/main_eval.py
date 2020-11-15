bcc9ae1fefb66a62e6ace94d22724e97baee3383
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-16 00:00:28 +0800

    synthetic data

--- synthetic/main_eval.py
+++ synthetic/main_eval.py
@@ -121,8 +121,10 @@ class GLDisen(nn.Module):
                 z_l= None
                 for frac in [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]:
 
-                    x_new = torch.cat([x[:,0],x[:,1]* frac],0)
+                    x_new = torch.cat([x[:,0].qnsqueeze(-1),(x[:,1]* frac).qnsqueeze(-1)],-1)
                     print('x ', x.size(), x_new.size())
+
+                    print('data', x, x_new)
 
                     if input_g is None:
                         input_g = x_new[:,1]
