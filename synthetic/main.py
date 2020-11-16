24a5f1f3cf03de5a42c27eae6cf2f451001c2be9
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-16 10:05:56 +0800

    synthetic data

--- synthetic/main.py
+++ synthetic/main.py
@@ -64,7 +64,7 @@ class GLDisen(nn.Module):
             - 0.5 * torch.sum(1 + node_logvar - node_mu.pow(2) - node_logvar.exp())
         )
         #node_kl_divergence_loss = 0.0000001 * node_kl_divergence_loss *num_graphs
-        node_kl_divergence_loss = 0.0001 *node_kl_divergence_loss
+        node_kl_divergence_loss = 10000 *node_kl_divergence_loss
         node_kl_divergence_loss.backward(retain_graph=True)
 
         # kl-divergence error for class latent space
@@ -72,7 +72,7 @@ class GLDisen(nn.Module):
             - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
         )
         #class_kl_divergence_loss = 0.0000001 * class_kl_divergence_loss * num_graphs
-        class_kl_divergence_loss =  0.0001 *class_kl_divergence_loss
+        class_kl_divergence_loss =  10000 * *class_kl_divergence_loss
         class_kl_divergence_loss.backward(retain_graph=True)
 
         # reconstruct samples
