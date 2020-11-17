b5f00853ffcef0a9df556db5ef1717ade591cad3
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-17 14:18:49 +0800

    synthetic data

--- Cora/intuitive_pubmed_eval.py
+++ Cora/intuitive_pubmed_eval.py
@@ -217,7 +217,7 @@ class GcnInfomax(nn.Module):
             node_mu, node_logvar, class_mu, class_logvar = self.encoder(x.double(), edge_index)
 
 
-            global_latent_all = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
+            global_latent_all = reparameterize(training=False, mu=class_mu, logvar=class_logvar)
 
         train_targets = global_latent_all[data.train_mask].cpu().numpy()
         test_targets = global_latent_all[data.test_mask].cpu().numpy()
@@ -529,9 +529,9 @@ if __name__ == '__main__':
         regr.fit(train_feat, train_targets)
 
 
-        predic = regr.predict(test_feat)
+        predic = regr.score(test_feat, test_targets)
 
-        print('ored ', predic.shape, test_targets.shape)
+        print('ored ', predic, predic.shape, test_targets.shape)
 
         #coef = regr.feature_importances_
 
