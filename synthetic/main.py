f47d02fc4bd27f9db5b429c224b48e6b2d7b9f22
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-19 14:41:43 +0800

    synthetic data

--- synthetic/main.py
+++ synthetic/main.py
@@ -282,7 +282,7 @@ if __name__ == '__main__':
 
     dataset = SyntheticERDataset(path).shuffle()
 
-    train_dataset = dataset[:3000]
+    train_dataset = dataset[:1000]
     test_dataset = dataset[3000:]
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
