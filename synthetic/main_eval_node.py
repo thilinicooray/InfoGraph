2a4454c08655d1dca4da289a2bc991b8decbbcf0
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-19 18:24:41 +0800

    synthetic data

--- synthetic/main_eval_node.py
+++ synthetic/main_eval_node.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
     print('num_gc_layers: {}'.format(args.num_gc_layers))
     print('================')
 
-    model.load_state_dict(torch.load(f'syner_n_model_correct2_big_ep30_all3000.pkl'))
+    model.load_state_dict(torch.load(f'syner_n_model_correct2_big_ep30_all3000_unshuf.pkl'))
 
 
     '''model.train()
