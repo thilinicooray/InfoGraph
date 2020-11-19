82d070967b51de734805f243564076ca5a9a929c
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-19 18:16:47 +0800

    synthetic data

--- synthetic/main_eval_node.py
+++ synthetic/main_eval_node.py
@@ -202,10 +202,10 @@ class GLDisen(nn.Module):
         local2 = np.concatenate(local2, 0)
         probability = np.concatenate(probability, 0)
 
-        savetxt('global_rep_tot.csv', global_rep, delimiter=',')
-        savetxt('local1_tot.csv', local1, delimiter=',')
-        savetxt('local2_tot.csv', local2, delimiter=',')
-        savetxt('probability_tot.csv', probability, delimiter=',')
+        savetxt('global_rep_tot_big.csv', global_rep, delimiter=',')
+        savetxt('local1_tot_big.csv', local1, delimiter=',')
+        savetxt('local2_tot_big.csv', local2, delimiter=',')
+        savetxt('probability_tot_big.csv', probability, delimiter=',')
 
 
         '''ret = np.concatenate(ret, 0)
@@ -265,7 +265,7 @@ if __name__ == '__main__':
     print('num_gc_layers: {}'.format(args.num_gc_layers))
     print('================')
 
-    model.load_state_dict(torch.load(f'syner_n_model_correct2_ep30_all3000.pkl'))
+    model.load_state_dict(torch.load(f'syner_n_model_correct2_big_ep30_all3000.pkl'))
 
 
     '''model.train()
