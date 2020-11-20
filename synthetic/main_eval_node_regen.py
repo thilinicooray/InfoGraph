4644267d29a721235b6bec5a5f3fd338599c06a8
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-20 20:54:47 +0800

    synthetic data

--- synthetic/main_eval_node_regen.py
+++ synthetic/main_eval_node_regen.py
@@ -159,9 +159,8 @@ class GLDisen(nn.Module):
 
                 from sklearn.preprocessing import MinMaxScaler
                 scaler = MinMaxScaler()
-                np_rec_adj = np.squeeze(np_rec_adj).transpose()
                 scaler.fit(np_rec_adj)
-                long_adj = scaler.transform(np_rec_adj)
+                long_adj = scaler.transform(np_rec_adj_lin)
 
                 print('long scale ', long_adj.shape, long_adj)
 
