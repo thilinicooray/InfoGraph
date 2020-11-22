e7d503e28db7e14cd4201bd7e2a1553f6eba4f62
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-22 15:24:05 +0800

    synthetic data

--- synthetic/er_try.py
+++ synthetic/er_try.py
@@ -5,24 +5,40 @@ from torch_geometric.utils import negative_sampling, remove_self_loops, add_self
 
 
 
+seed = 1234
+
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+os.environ['PYTHONHASHSEED'] = str(seed)
+
+
+
 num_nodes = 50
-edge_prob = 0.1
+edge_prob_list = [0.1, 0.3, 0.5, 0.7, 0.9]
 
-idx = torch.combinations(torch.arange(num_nodes))
+for i in range(10):
 
+    for edge_prob in edge_prob_list:
 
-a = torch.rand(idx.size(0))
-
-
-mask = a  < edge_prob
-
-print('mask', mask.size(),torch.sum(mask), torch.sum(mask)/1125)
-
-idx = idx[mask]
+        idx = torch.combinations(torch.arange(num_nodes))
 
 
-edge_index = to_undirected(idx.t(), num_nodes)
+        a = torch.rand(idx.size(0))
 
-org_adj = to_dense_adj(edge_index)
 
-print('adj ', torch.sum(org_adj))
+        mask = a  < edge_prob
+
+        print('mask', mask.size(),torch.sum(mask), torch.sum(mask)/1125)
+
+        idx = idx[mask]
+
+
+        edge_index = to_undirected(idx.t(), num_nodes)
+
+        org_adj = to_dense_adj(edge_index)
+
+        print('adj ',i, torch.sum(org_adj),torch.sum(org_adj)/idx.size(0) , edge_prob)
