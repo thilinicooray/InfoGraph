30338c19be59e91b50bbe656d12fd041262ac54c
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-11-22 17:09:21 +0800

    synthetic data

--- synthetic/main_eval_10_regen.py
+++ synthetic/main_eval_10_regen.py
@@ -193,7 +193,9 @@ class GLDisen(nn.Module):
                             current_cut = np_rec_adj[k][m]
                             mask = (np_rec_adj > current_cut).astype(int)
 
-                            p_gen = np.sum(mask)/tot_edges
+                            mask1 = np.tril((mask),k=-1)
+
+                            p_gen = np.sum(mask1)/tot_edges
 
                             err = np.power(pr - p_gen,2)
 
@@ -262,9 +264,9 @@ class GLDisen(nn.Module):
         org = np.concatenate(org_p, 0)
         #regen_adj = np.concatenate(regen_adj, 0)
 
-        savetxt('global_rep_tot_50_regen1_small.csv', global_rep, delimiter=',')
-        savetxt('regen_p1_small.csv', regen, delimiter=',')
-        savetxt('org_p1_small.csv', org, delimiter=',')
+        savetxt('global_rep_tot_50_regen1_small_correct.csv', global_rep, delimiter=',')
+        savetxt('regen_p1_small_correct.csv', regen, delimiter=',')
+        savetxt('org_p1_small_correct.csv', org, delimiter=',')
         #savetxt('regen1_adj_small.csv', regen_adj, delimiter=',')
 
 
