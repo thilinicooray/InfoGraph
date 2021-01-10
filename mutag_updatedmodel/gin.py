c42f84cb5edcbc366a6471734e3c47ac2a8efff1
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-11 07:43:33 +0800

    update enc

--- mutag_updatedmodel/gin.py
+++ mutag_updatedmodel/gin.py
@@ -79,8 +79,8 @@ class Encoder(torch.nn.Module):
         global_n = global_add_pool(global_weights*x, batch)
 
         j = self.num_gc_layers
-        node_latent_space_mu = self.bns[j](F.relu(self.convs[j](x, edge_index)))
-        node_latent_space_logvar = self.bns[j+1](F.relu(self.convs[j+1](x, edge_index)))
+        node_latent_space_mu = self.bns[j](F.relu(self.convs[j]((1-global_weights)*x, edge_index)))
+        node_latent_space_logvar = self.bns[j+1](F.relu(self.convs[j+1]((1-global_weights)*x, edge_index)))
 
         class_latent_space_mu = self.bns[j+2](F.relu(self.cls_mu(global_n)))
         class_latent_space_logvar = self.bns[j+3](F.relu(self.cls_logv(global_n)))
