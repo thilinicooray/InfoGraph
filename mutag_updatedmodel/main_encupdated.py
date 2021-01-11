7405185e160dc108edfccd4cbeb7d113fda93915
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-11 19:24:32 +0800

    update enc

--- mutag_updatedmodel/main_encupdated.py
+++ mutag_updatedmodel/main_encupdated.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     seeds = [32,42,52,62,72]
 
     #seeds = [123,132,213,231,312,321] this set also give similar results
-    epochs_list =[20,30,40,50, 100]
+    epochs_list =[20,30,40,50, 100, 200, 300]
     node_ratio = [0.25,0.5,0.75]
     for seed in seeds:
         for epochs in epochs_list:
