5b7f281373500fcce280f23c6ef3952f5dc376f4
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-11 08:58:22 +0800

    update enc

--- mutag_updatedmodel/gin.py
+++ mutag_updatedmodel/gin.py
@@ -103,7 +103,7 @@ class Decoder(torch.nn.Module):
     def forward(self, node_latent_space, class_latent_space):
         x = torch.cat((node_latent_space, class_latent_space), dim=1)
 
-        x = torch.softmax(self.linear_model(x), dim=-1)
+        x = self.linear_model(x)
 
         return x
 
