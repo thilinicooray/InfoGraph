9cf38e8107455b01e95b6aa49d750b8f26f5dab4
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-14 15:53:10 +0800

    update enc

--- ptc_updatedmodel/main_encupdated_distil.py
+++ ptc_updatedmodel/main_encupdated_distil.py
@@ -145,7 +145,7 @@ class GLDisen(nn.Module):
 
         mask = recon_adj >= 0.5
 
-        print('sizes ', mask.size(), recon_adj.size(), edge_index.size())
+        print('sizes ', mask.size(), recon_adj.size(), edge_index.size(), mask.sum())
 
         new_adj = edge_index
         new_adj[0] = torch.masked_select(edge_index[0], mask)
