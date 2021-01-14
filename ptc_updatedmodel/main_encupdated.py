8dcbd2fe23444b7ddb3d82fb5a9d72d3ab507689
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-14 14:25:19 +0800

    update enc

--- ptc_updatedmodel/main_encupdated.py
+++ ptc_updatedmodel/main_encupdated.py
@@ -202,7 +202,13 @@ class GLDisen(nn.Module):
                 recon_adj = self.edge_recon(reconstructed_node, edge_index)
 
                 mask = recon_adj >= 0.5
-                print('mask ', mask, mask.size(), recon_adj.size())
+
+                new_adj = edge_index
+                new_adj[0] = torch.masked_select(edge_index[0], mask)
+                new_adj[1] = torch.masked_select(edge_index[1], mask)
+
+
+                print('mask ', mask, mask.size(), recon_adj.size(), new_adj.size())
 
 
 
