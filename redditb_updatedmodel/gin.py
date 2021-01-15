8c46a5dc107ecce7c197349f7ac4d886897dfabd
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-15 11:05:06 +0800

    update enc

---
+++ redditb_updatedmodel/gin.py
@@ -0,0 +1,180 @@
+import os.path as osp
+from tqdm import tqdm
+from collections import OrderedDict
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Sequential, Linear, ReLU, Tanh
+import torch.nn as nn
+from torch_geometric.datasets import TUDataset
+from torch_geometric.data import DataLoader
+from torch_geometric.nn import GINConv, global_add_pool
+
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV, KFold, StratifiedKFold
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool
+from torch.nn.utils.weight_norm import weight_norm
+import sys
+import math
+
+class Encoder(torch.nn.Module):
+    def __init__(self, num_features, dim, num_gc_layers, node_dim, class_dim):
+        super(Encoder, self).__init__()
+
+        # num_features = dataset.num_features
+        # dim = 32
+        self.num_gc_layers = num_gc_layers
+
+        # self.nns = []
+        self.convs = torch.nn.ModuleList()
+        self.bns = torch.nn.ModuleList()
+
+        for i in range(num_gc_layers+4):
+
+            if i == 0:
+                nn = Sequential(Linear(num_features, dim), ReLU(), Linear(dim, dim))
+                bn = torch.nn.BatchNorm1d(dim)
+            elif i >= num_gc_layers and i < num_gc_layers +2:
+                nn = Sequential(Linear(dim, dim), ReLU(), Linear(dim, node_dim))
+                bn = torch.nn.BatchNorm1d(node_dim)
+            elif i >= num_gc_layers and i >= num_gc_layers +2:
+                nn = Sequential(Linear(dim, dim), ReLU(), Linear(dim, class_dim))
+                bn = torch.nn.BatchNorm1d(class_dim)
+            else:
+                nn = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+                bn = torch.nn.BatchNorm1d(dim)
+            conv = GINConv(nn)
+
+            self.convs.append(conv)
+            self.bns.append(bn)
+
+        self.att = Sequential(Linear(dim, dim), ReLU(), Linear(dim, 1))
+
+        self.cls_mu = Sequential(Linear(dim, dim), ReLU(), Linear(dim, class_dim))
+        self.cls_logv = Sequential(Linear(dim, dim), ReLU(), Linear(dim, class_dim))
+
+
+
+    def forward(self, x, edge_index, batch):
+        global_n = None
+        if x is None:
+            x = torch.ones((batch.shape[0], 1)).to(device)
+
+        xs = []
+        for i in range(self.num_gc_layers):
+
+            x = F.relu(self.convs[i](x, edge_index))
+            x = self.bns[i](x)
+
+            xs.append(x)
+            # if i == 2:
+                # feature_map = x2
+        global_weights = torch.sigmoid(self.att(x))
+        global_n = global_add_pool(global_weights*x, batch)
+
+        j = self.num_gc_layers
+        node_latent_space_mu = self.bns[j](F.relu(self.convs[j]((1-global_weights)*x, edge_index)))
+        node_latent_space_logvar = self.bns[j+1](F.relu(self.convs[j+1]((1-global_weights)*x, edge_index)))
+
+        class_latent_space_mu = self.bns[j+2](F.relu(self.cls_mu(global_n)))
+        class_latent_space_logvar = self.bns[j+3](F.relu(self.cls_logv(global_n)))
+
+        return node_latent_space_mu, node_latent_space_logvar, class_latent_space_mu, class_latent_space_logvar
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, node_dim, class_dim, feat_size):
+        super(Decoder, self).__init__()
+
+        self.linear_model = torch.nn.Sequential(OrderedDict([
+            ('linear_1', torch.nn.Linear(in_features=node_dim + class_dim, out_features=node_dim, bias=True)),
+            ('relu_1', ReLU()),
+
+            ('linear_2', torch.nn.Linear(in_features=node_dim, out_features=feat_size, bias=True)),
+            ('relu_final', ReLU()),
+        ]))
+
+    def forward(self, node_latent_space, class_latent_space):
+        x = torch.cat((node_latent_space, class_latent_space), dim=1)
+
+        x = self.linear_model(x)
+
+        return x
+
+class FF(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        # self.c0 = nn.Conv1d(input_dim, 512, kernel_size=1)
+        # self.c1 = nn.Conv1d(512, 512, kernel_size=1)
+        # self.c2 = nn.Conv1d(512, 1, kernel_size=1)
+        self.block = nn.Sequential(
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU(),
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU(),
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU()
+        )
+        self.linear_shortcut = nn.Linear(input_dim, input_dim)
+        # self.c0 = nn.Conv1d(input_dim, 512, kernel_size=1, stride=1, padding=0)
+        # self.c1 = nn.Conv1d(512, 512, kernel_size=1, stride=1, padding=0)
+        # self.c2 = nn.Conv1d(512, 1, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        return self.block(x) + self.linear_shortcut(x)
+
+
+
+if __name__ == '__main__':
+
+    for percentage in [ 1.]:
+        for DS in [sys.argv[1]]:
+            if 'REDDIT' in DS:
+                epochs = 200
+            else:
+                epochs = 100
+            path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', DS)
+            accuracies = [[] for i in range(epochs)]
+            #kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
+            dataset = TUDataset(path, name=DS) #.shuffle()
+            num_graphs = len(dataset)
+            print('Number of graphs', len(dataset))
+            dataset = dataset[:int(num_graphs * percentage)]
+            dataset = dataset.shuffle()
+
+            kf = KFold(n_splits=10, shuffle=True, random_state=None)
+            for train_index, test_index in kf.split(dataset):
+
+                # x_train, x_test = x[train_index], x[test_index]
+                # y_train, y_test = y[train_index], y[test_index]
+                train_dataset = [dataset[int(i)] for i in list(train_index)]
+                test_dataset = [dataset[int(i)] for i in list(test_index)]
+                print('len(train_dataset)', len(train_dataset))
+                print('len(test_dataset)', len(test_dataset))
+
+                train_loader = DataLoader(train_dataset, batch_size=128)
+                test_loader = DataLoader(test_dataset, batch_size=128)
+                # print('train', len(train_loader))
+                # print('test', len(test_loader))
+
+                device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                model = Net().to(device)
+                optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+                for epoch in range(1, epochs+1):
+                    train_loss = train(epoch)
+                    train_acc = test(train_loader)
+                    test_acc = test(test_loader)
+                    accuracies[epoch-1].append(test_acc)
+                    tqdm.write('Epoch: {:03d}, Train Loss: {:.7f}, '
+                          'Train Acc: {:.7f}, Test Acc: {:.7f}'.format(epoch, train_loss,
+                                                                       train_acc, test_acc))
+            tmp = np.mean(accuracies, axis=1)
+            print(percentage, DS, np.argmax(tmp), np.max(tmp), np.std(accuracies[np.argmax(tmp)]))
+            input()
