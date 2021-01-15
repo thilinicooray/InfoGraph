38eb9f39b3e883dc87384f153c984ed3c0c714aa
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-15 15:42:39 +0800

    update enc

--- redditb_updatedmodel/evaluate_embedding.py
+++ redditb_updatedmodel/evaluate_embedding.py
@@ -91,32 +91,38 @@ def logistic_classify(x, y):
     return np.mean(accs)
 
 def svc_classify(x, y, search):
-    tot_acc = []
 
-    for _ in range(5):
-        kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
-        accuracies = []
-        for train_index, test_index in kf.split(x, y):
 
-            x_train, x_test = x[train_index], x[test_index]
-            y_train, y_test = y[train_index], y[test_index]
-            # x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)#to find best c
-            if search:
-                params = {'C':[0.001, 0.01,0.1,1,10,100,1000]}
-                classifier = GridSearchCV(SVC(), params, cv=5, scoring='accuracy', verbose=0)
-            else:
-                classifier = SVC(C=10)
-            classifier.fit(x_train, y_train)
-            accuracies.append(accuracy_score(y_test, classifier.predict(x_test)))
+    C = [0.001, 0.01,0.1,1,10,100,1000,10000]
 
-        mean = np.mean(accuracies)
-        std = np.std(accuracies)
+    for c in C:
 
-        tot_acc.append(mean)
+        tot_acc = []
 
-        print('mean libsvm ', mean, 'std ', std)
+        for _ in range(5):
+            kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=None)
+            accuracies = []
+            for train_index, test_index in kf.split(x, y):
 
-    print('mean of 5 runs', np.mean(tot_acc), np.std(tot_acc))
+                x_train, x_test = x[train_index], x[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+                # x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)#to find best c
+                if search:
+                    params = {'C':[0.001, 0.01,0.1,1,10,100,1000]}
+                    classifier = GridSearchCV(SVC(), params, cv=5, scoring='accuracy', verbose=0)
+                else:
+                    classifier = SVC(C=c)
+                classifier.fit(x_train, y_train)
+                accuracies.append(accuracy_score(y_test, classifier.predict(x_test)))
+
+            mean = np.mean(accuracies)
+            std = np.std(accuracies)
+
+            tot_acc.append(mean)
+
+            #print('mean libsvm ', mean, 'std ', std)
+
+        print('mean of 5 runs', c, np.mean(tot_acc), np.std(tot_acc))
 
     return np.mean(tot_acc)
 
