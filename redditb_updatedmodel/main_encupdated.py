ca7898bb05a9d47cf5dd2c63c715feb6157a9648
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-15 11:46:21 +0800

    update enc

--- redditb_updatedmodel/main_encupdated.py
+++ redditb_updatedmodel/main_encupdated.py
@@ -65,7 +65,7 @@ class GLDisen(nn.Module):
             1 + 2 * node_logvar - node_mu.pow(2) - node_logvar.exp().pow(2), 1))
 
         #node_kl_divergence_loss = 0.0000001 * node_kl_divergence_loss *num_graphs
-        node_kl_divergence_loss = 1e6*node_kl_divergence_loss
+        node_kl_divergence_loss = node_kl_divergence_loss
         #node_kl_divergence_loss.backward(retain_graph=True)
 
         # kl-divergence error for class latent space
@@ -75,7 +75,7 @@ class GLDisen(nn.Module):
         class_kl_divergence_loss = -0.5 / n_nodes * torch.mean(torch.sum(
             1 + 2 * class_logvar - class_mu.pow(2) - class_logvar.exp().pow(2), 1))
         #class_kl_divergence_loss = 0.0000001 * class_kl_divergence_loss * num_graphs
-        class_kl_divergence_loss = 1e3*class_kl_divergence_loss
+        class_kl_divergence_loss = class_kl_divergence_loss
         #class_kl_divergence_loss.backward(retain_graph=True)
 
         # reconstruct samples
