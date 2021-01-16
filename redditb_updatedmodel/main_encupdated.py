e1a3e4fbb214ddebee483bbba9f7917520f6f220
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-16 10:55:54 +0800

    update enc

--- redditb_updatedmodel/main_encupdated.py
+++ redditb_updatedmodel/main_encupdated.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     #enable entire sets of hyperparameters for the full experiment
 
     #seeds = [32,42,52,62,72]
-    seeds = [32, 42]
+    seeds = [32, 42,52,62,72]
 
     #seeds = [123,132,213,231,312,321] #this set also give similar results
     epochs_list =[25,50,75,100]
