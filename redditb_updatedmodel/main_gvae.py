ac1dd9f89e145619fe9ea0b3eea96a335675f816
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-16 10:52:31 +0800

    update enc

--- redditb_updatedmodel/main_gvae.py
+++ redditb_updatedmodel/main_gvae.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     seeds = [32,42,52,62,72]
 
     #seeds = [123,132,213,231,312,321] #this set also give similar results
-    epochs_list =[1000, 50, 75, 100]
+    epochs_list =[300]
     node_ratio = [0.5]
     for seed in seeds:
         for epochs in epochs_list:
