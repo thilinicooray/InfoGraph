84c5c863e9e0d0c479d27d3d5b320739665fe225
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-19 11:39:57 +0800

    update enc

--- imdbb_updatedmodel/main_encupdated.py
+++ imdbb_updatedmodel/main_encupdated.py
@@ -170,12 +170,12 @@ class GLDisen(nn.Module):
                 if not dataset.num_features:
                     x = torch.ones((data.batch.shape[0], 5)).to(device)
                 node_mu, node_logvar , class_mu, class_logvar = self.encoder(x, edge_index, batch)
-                class_emb = reparameterize(training=False, mu=class_mu, logvar=class_logvar)
-                #mean_mu = global_mean_pool(node_mu, batch)
-                #mean_logvar = global_mean_pool(node_logvar, batch)
+                #class_emb = reparameterize(training=False, mu=class_mu, logvar=class_logvar)
+                mean_mu = global_mean_pool(node_mu, batch)
+                mean_logvar = global_mean_pool(node_logvar, batch)
                 #node_emb = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
                 #class_emb = global_mean_pool(node_emb, batch)
-                #class_emb = reparameterize(training=False, mu=mean_mu, logvar=mean_logvar)
+                class_emb = reparameterize(training=False, mu=mean_mu, logvar=mean_logvar)
 
                 ret.append((class_emb).cpu().numpy())
                 y.append(data.y.cpu().numpy())
