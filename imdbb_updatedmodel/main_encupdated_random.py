73b9397c5c577b96dca37c3f5ef46957b62823cf
thilinicooray <thilinicooray.ucsc@gmail.com>
2021-01-19 09:28:42 +0800

    update enc

--- imdbb_updatedmodel/main_encupdated_random.py
+++ imdbb_updatedmodel/main_encupdated_random.py
@@ -178,12 +178,12 @@ class GLDisen(nn.Module):
                     y_onehot.scatter_(1, y_oh, 1)
                     x = y_onehot
                 node_mu, node_logvar , class_mu, class_logvar = self.encoder(x, edge_index, batch)
-                #class_emb = reparameterize(training=False, mu=class_mu, logvar=class_logvar)
-                mean_mu = global_mean_pool(node_mu, batch)
-                mean_logvar = global_mean_pool(node_logvar, batch)
+                class_emb = reparameterize(training=False, mu=class_mu, logvar=class_logvar)
+                #mean_mu = global_mean_pool(node_mu, batch)
+                #mean_logvar = global_mean_pool(node_logvar, batch)
                 #node_emb = reparameterize(training=False, mu=node_mu, logvar=node_logvar)
                 #class_emb = global_mean_pool(node_emb, batch)
-                class_emb = reparameterize(training=False, mu=mean_mu, logvar=mean_logvar)
+                #class_emb = reparameterize(training=False, mu=mean_mu, logvar=mean_logvar)
 
                 ret.append((class_emb).cpu().numpy())
                 y.append(data.y.cpu().numpy())
